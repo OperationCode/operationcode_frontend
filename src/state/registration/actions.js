cfcab85b1d9a3bb8347f0df80298d102f3e51439
Michael Kirschbaum <me@michaelkirschbaum.com>
2016-12-31 16:06:53 -0800

    add actions

--- src/state/registration/actions.js
+++ src/state/registration/actions.js
@@ -1,3 +1,11 @@
 export function addEmail(email) {
   return { type: 'ADD_EMAIL', email }
 }
+
+export function addPassword(password) {
+  return { type: 'ADD_PASSWORD', password }
+}
+
+export function addZip(zip) {
+  return { type: 'ADD_ZIP', zip }
+}
