29bdf65213ea0917f5833a050145c4e45a8fbfa0
Alexander Spence <aspence@smashglass.com>
2017-05-01 22:34:48 -0500

    Change default port to 4000

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.get('/*', function (req, res) {
    res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 9000);
+app.listen(process.env.PORT || 4000);
