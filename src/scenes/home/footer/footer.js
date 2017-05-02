d0bfb190b4def3858d4ceb6210bf7ffa08496384
John Hampton <john_j_hampton@yahoo.com>
2017-05-02 01:36:51 -0400

    Adjust responsive styles for footer
    Add media query rules for social media icons & page links at sizes larger than mobile phone

--- src/scenes/home/footer/footer.js
+++ src/scenes/home/footer/footer.js
@@ -8,25 +8,27 @@ class Footer extends Component {
   render() {
     return (
       <div className={styles.footer}>
-        <div className={styles.left}>
-          <div className={styles.smContactFooterContainer}>
-            <div className={styles.contact} >
+        <div className={styles.content}>
+          <div className={`${styles.block} ${styles.contactLinks}`}>
+            <div className={styles.email} >
               <a href="mailto:contact@operationcode.org">contact@operationcode.org</a>
             </div>
             <SocialMedia />
           </div>
-        </div>
-        <div className={styles.center}>
-          <img src={centerLogo} alt="" />
-        </div>
-        <div className={styles.right}>
-          <div className={styles.nav} >
-            <Link to="about">About&nbsp;Us</Link>
-            <Link to="programs">Our&nbsp;Programs</Link>
-            <Link to="involved">Our&nbsp;Partners</Link>
-            <Link to="involved">Get&nbsp;Involved</Link>
-            <Link to="blog">Blog</Link>
-            <Link to="contact">Contact</Link>
+          <div className={styles.logo}>
+            <img src={centerLogo} alt="" />
+          </div>
+          <div className={`${styles.block} ${styles.pageLinks}`}>
+            <div className={styles.blockGroup} >
+              <Link to="about">About&nbsp;Us</Link>
+              <Link to="programs">Our&nbsp;Programs</Link>
+              <Link to="involved">Our&nbsp;Partners</Link>
+            </div>
+            <div className={styles.blockGroup} >
+              <Link to="involved">Get&nbsp;Involved</Link>
+              <Link to="blog">Blog</Link>
+              <Link to="contact">Contact</Link>
+            </div>
           </div>
         </div>
       </div>
