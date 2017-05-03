1642fe4ee108cf034cb59372ff23254eab5115b6
Alexander Spence <aspence@smashglass.com>
2017-05-03 11:13:36 -0500

    membership updates.

--- src/scenes/home/landing/membership/membership.js
+++ src/scenes/home/landing/membership/membership.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
 import Section from 'shared/components/section/section';
 import styles from './membership.css';
 
 class Membership extends Component {
   render() {
     return (
+
       <Section title="Membership" theme="white">
+        <FontAwesome name="flag" className={styles.flag} />
         <h3 className={styles.heading}>Joining Operation Code is easy and free!  Once
           you&#39;re signed up you can join us on Slack and start enjoying the
            benefits of your membership:
         </h3>
+
         <ul className={styles.list}>
           <li>
+            <FontAwesome name="flag-checkered" />
             Join an open community of veterans,
             non-veterans, developers, and learners who you can engage and grow with
           </li>
