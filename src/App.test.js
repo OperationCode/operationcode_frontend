b80bf590354541dc4763516edc994deff7479fdf
RickR <rrein@akamai.com>
2017-05-05 22:17:14 -0600

    Turns tests back on

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-// This import and the subsequent test fail to run with the
-// following error:
-//
-// Cannot find module 'shared/components/login/login' from 'home.js'
-//
-// If you fix this load error using a relative path more show up.
-//
-//import App from './App';
-
-//it('renders without crashing', () => {
-//  const div = document.createElement('div');
-//  ReactDOM.render(<App />, div);
-//});
-
-it('can run a test', () => {
-  1 + 1 == 2
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
