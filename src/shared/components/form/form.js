6adf7278e5b0315f44d9da50588da623651358e5
John Hampton <john_j_hampton@yahoo.com>
2017-05-07 17:28:41 -0400

    Add CSS styling for dataFormItem and Form components
    Added text color, placeholder color, & layout properties

--- src/shared/components/form/form.js
+++ src/shared/components/form/form.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styles from './form.css';
+
 
 class Form extends Component {
   render() {
     return (
-      <form>
+      <form className={styles.form}>
         {this.props.children}
       </form>
     );
