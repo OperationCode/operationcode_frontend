1c1b28f950596616398526f7a96e68425104b422
Kyle Holmberg <kylemh.email@gmail.com>
2017-05-08 15:25:25 -0700

    Add links to social media icons

--- src/shared/components/socialMedia/socialMedia.js
+++ src/shared/components/socialMedia/socialMedia.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import fbImage from 'images/icons/Facebook-Icon.svg';
 import ghImage from 'images/icons/GitHub-Icon.svg';
 import twtImage from 'images/icons/Twitter-Icon.svg';
@@ -10,10 +11,18 @@ class SocialMedia extends Component {
   render() {
     return (
       <SocialMediaContainer>
-        <SocialMediaItem smImage={fbImage} smText="Facebook" />
-        <SocialMediaItem smImage={ghImage} smText="Github" />
-        <SocialMediaItem smImage={twtImage} smText="Twitter" />
-        <SocialMediaItem smImage={liImage} smText="LinkedIn" />
+        <Link to="https://facebook.com/operationcode.org">
+          <SocialMediaItem smImage={fbImage} smText="Facebook" />
+        </Link>
+        <Link to="https://github.com/operationcode">
+          <SocialMediaItem smImage={ghImage} smText="Github" />
+        </Link>
+        <Link to="https://twitter.com/operation_code">
+          <SocialMediaItem smImage={twtImage} smText="Twitter" />
+        </Link>
+        <Link to="https://www.linkedin.com/groups/13400924">
+          <SocialMediaItem smImage={liImage} smText="LinkedIn" />
+        </Link>
       </SocialMediaContainer>
     );
   }
