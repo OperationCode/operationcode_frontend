597c11056865d02f3e565bbdc18699a38efefa94
John Hampton <john_j_hampton@yahoo.com>
2017-05-08 00:00:12 -0400

    Navigate to Donorbox page when nav item clicked

--- src/shared/components/nav/navItem/navItem.js
+++ src/shared/components/nav/navItem/navItem.js
@@ -4,9 +4,25 @@ import PropTypes from 'prop-types';
 import styles from './navItem.css';
 
 class NavItem extends PureComponent {
+
+  constructor() {
+    super();
+
+    // remove this if .eslintrc updated to parser:"babel-eslint", allowing class methods as => fns
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    if (!this.props.isExternal) {
+      return;
+    }
+
+    window.location(this.props.to);
+  }
+
   render() {
     return (
-      <Link className={styles.navItem} to={this.props.to}>
+      <Link className={styles.navItem} to={this.props.to} onClick={this.handleClick}>
         {this.props.text}
       </Link>
     );
@@ -14,8 +30,13 @@ class NavItem extends PureComponent {
 }
 
 NavItem.propTypes = {
+  isExternal: PropTypes.bool,
   to: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired
+};
+
+NavItem.defaultProps = {
+  isExternal: false
 };
 
 export default NavItem;
