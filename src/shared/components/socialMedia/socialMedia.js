63c1734a9a2b891a01b26523cf4285d14ce2a195
Kyle Holmberg <kylemh.email@gmail.com>
2017-05-08 18:00:31 -0700

    Refactor social media link implementation

--- src/shared/components/socialMedia/socialMedia.js
+++ src/shared/components/socialMedia/socialMedia.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import fbImage from 'images/icons/Facebook-Icon.svg';
 import ghImage from 'images/icons/GitHub-Icon.svg';
 import twtImage from 'images/icons/Twitter-Icon.svg';
@@ -11,18 +10,10 @@ class SocialMedia extends Component {
   render() {
     return (
       <SocialMediaContainer>
-        <Link to="https://facebook.com/operationcode.org">
-          <SocialMediaItem smImage={fbImage} smText="Facebook" />
-        </Link>
-        <Link to="https://github.com/operationcode">
-          <SocialMediaItem smImage={ghImage} smText="Github" />
-        </Link>
-        <Link to="https://twitter.com/operation_code">
-          <SocialMediaItem smImage={twtImage} smText="Twitter" />
-        </Link>
-        <Link to="https://www.linkedin.com/groups/13400924">
-          <SocialMediaItem smImage={liImage} smText="LinkedIn" />
-        </Link>
+        <SocialMediaItem smImage={fbImage} smText="Facebook" link="https://facebook.com/operationcode.org" />
+        <SocialMediaItem smImage={ghImage} smText="Github" link="https://github.com/operationcode" />
+        <SocialMediaItem smImage={twtImage} smText="Twitter" link="https://twitter.com/operation_code" />
+        <SocialMediaItem smImage={liImage} smText="LinkedIn" link="https://www.linkedin.com/groups/13400924" />
       </SocialMediaContainer>
     );
   }
--- src/shared/components/socialMedia/socialMediaItem/socialMediaItem.js
+++ src/shared/components/socialMedia/socialMediaItem/socialMediaItem.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './socialMediaItem.css';
 
 const SocialMediaItem = (props) => {
   const {
     smImage,
-    smText
+    smText,
+    link
   } = props;
+
   return (
     <div className={styles.socialMediaItem}>
-      <img src={smImage} alt={smText} />
+      <Link to={link}><img src={smImage} alt={smText} /></Link>
     </div>
   );
 };
 
 SocialMediaItem.propTypes = {
   smImage: PropTypes.string.isRequired,
-  smText: PropTypes.string.isRequired
+  smText: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired
 };
 
 export default SocialMediaItem;
