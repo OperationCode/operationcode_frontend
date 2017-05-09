8fddaab8b55595b2e5653c7fe6ca9b12bcee0c22
Kyle Holmberg <kylemh.email@gmail.com>
2017-05-08 19:29:19 -0700

    Implement optional button-link functionality for ImageCards

--- src/shared/components/imageCard/imageCard.js
+++ src/shared/components/imageCard/imageCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import LinkButton from 'shared/components/linkButton/linkButton';
+import LinkButton from 'shared/components/linkButton/linkButton';
 import styles from './imageCard.css';
 
 class ImageCard extends Component {
@@ -12,6 +12,7 @@ class ImageCard extends Component {
           <div className={styles.cardText}>
             <h2>{this.props.title}:</h2>
             <p>{this.props.cardText}</p>
+            {this.props.link && <LinkButton text={this.props.buttonText} link={this.props.link} />}
           </div>
         </div>
       </div>
@@ -19,16 +20,20 @@ class ImageCard extends Component {
   }
 }
 
-// TODO - AFTER MVP:
-// Add within cardText div under the paragraph tag once links are generated
-// <LinkButton text={this.props.buttonText} link={this.props.link} />
-
 ImageCard.propTypes = {
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   cardText: PropTypes.string.isRequired,
-  // buttonText: PropTypes.string.isRequired,
-  // link: PropTypes.string.isRequired
+  buttonText: PropTypes.string,
+  link: PropTypes.string
 };
+
+ImageCard.defaultProps = {
+  link: null,
+  buttonText: null
+}
+
+// TODO: Uncomment buttons as routes are implemented
+// NOTE: ImageCard dynamically handles button-links as optionally, so no further work required
 
 export default ImageCard;
