72e1016697b114ba5ae6fc603441827c8e887d7f
Alexander Spence <aspence@smashglass.com>
2017-05-10 03:00:40 -0500

    Sign up page and component.  Fixes #35 and #27.

---
+++ src/scenes/home/thanks/thanks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Section from 'shared/components/section/section';
+
+class Thanks extends Component {
+  render() {
+    return (
+      <Section title="Thanks for joining">
+        <span>Thanks for joining operation code.  Please check your email for orders.</span>
+      </Section>
+    );
+  }
+}
+
+export default Thanks;
