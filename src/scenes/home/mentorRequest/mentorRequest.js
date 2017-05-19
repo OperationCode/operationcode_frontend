ed3fe9e7e04e76e3abc1028829df3ae8f0b8a091
mkmckenzie <mkmckenzie91@gmail.com>
2017-05-19 10:47:17 -0400

    add service type drop down for mentor request

--- src/scenes/home/mentorRequest/mentorRequest.js
+++ src/scenes/home/mentorRequest/mentorRequest.js
@@ -14,13 +14,34 @@ export default class MentorRequest extends Component {
     });
   }
 
+  onDetailsChange = (details) => {
+    this.setState({
+      additionalDetails: details
+    });
+  }
+
+  onServiceTypeChange = (service) => {
+    this.setState({
+      serviceType: service
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Mentor Service Request</h1>
         <Form>
           <FormInput id="slackName" onChange={this.onSlackNameChange} />
+          <select id="serviceType" value={this.state.serviceType} onChange={this.onServiceTypeChange}>
+            <option value="voiceChat">General Guidance - Voice Chat</option>
+            <option value="slackChat">General Guidance - Slack Chat</option>
+            <option value="pairProgramming">Pair Programming</option>
+            <option value="codeReview">Code Review</option>
+            <option value="mockInterview">Mock Interview</option>
+            <option value="resumeReview">Resume Review</option>
+          </select>
           <FormInput id="additionalDetails" onChange={this.onDetailsChange} />
+
         </Form>
       </div>
     );
