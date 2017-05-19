8d625850f8ef589cf65e5dae3e1ab792701ce413
Alex Egan <alexander.egan@customink.com>
2017-05-19 16:23:09 -0400

    Make authorized request to add a request

--- src/shared/utils/apiHelper.js
+++ src/shared/utils/apiHelper.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import config from 'config/environment';
+import Cookies from 'universal-cookie';
 
 function makeGenericGet(endpoint) {
   return axios.get(`${config.backendUrl}/${endpoint}`).then(({ data }) => data);
@@ -8,3 +9,25 @@ function makeGenericGet(endpoint) {
 export const getServices = () => makeGenericGet('services');
 
 export const getMentors = () => makeGenericGet('mentors');
+
+export const setAuthorizationHeader = () => {
+  const cookies = new Cookies();
+  return {
+    Authorization: `bearer ${cookies.get('token')}`
+  };
+};
+
+export function postRequest({ language, additionalDetails, mentor, service }) {
+  const authHeader = setAuthorizationHeader();
+
+  return axios.post(`${config.backendUrl}/requests`, {
+    request: {
+      details: additionalDetails,
+      requested_mentor_id: mentor,
+      service_id: service,
+      language
+    }
+  }, {
+    headers: authHeader
+  });
+}
