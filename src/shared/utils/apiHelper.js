0bde8519504c03490140c0c9452d91995430179d
Alex Egan <alexander.egan@customink.com>
2017-05-20 11:20:46 -0400

    Set auth header for GET /mentors and GET /services
    Now that we've locked down the mentors and services requests, this will
    add a header to those get requests to retrieve them for the mentor
    request form.
    
    This will also redirect to the login page from the mentor request page
    if the mentor request page is rendered and the user does not have an
    auth token.

--- src/shared/utils/apiHelper.js
+++ src/shared/utils/apiHelper.js
@@ -2,20 +2,23 @@ import axios from 'axios';
 import config from 'config/environment';
 import Cookies from 'universal-cookie';
 
-function makeGenericGet(endpoint) {
-  return axios.get(`${config.backendUrl}/${endpoint}`).then(({ data }) => data);
-}
-
-export const getServices = () => makeGenericGet('services');
-
-export const getMentors = () => makeGenericGet('mentors');
-
 export const setAuthorizationHeader = () => {
   const cookies = new Cookies();
   return {
     Authorization: `bearer ${cookies.get('token')}`
   };
 };
+
+function makeGenericGet(endpoint) {
+  const authHeader = setAuthorizationHeader();
+  return axios.get(`${config.backendUrl}/${endpoint}`, {
+    headers: authHeader
+  }).then(({ data }) => data);
+}
+
+export const getServices = () => makeGenericGet('services');
+
+export const getMentors = () => makeGenericGet('mentors');
 
 export function postRequest({ language, additionalDetails, mentor, service }) {
   const authHeader = setAuthorizationHeader();
