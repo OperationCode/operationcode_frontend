5fee3fdd92b7ec63710e7ae0d76d8aeae6a1b1d0
mkmckenzie <mkmckenzie91@gmail.com>
2017-05-20 15:26:19 -0400

    add squads route

--- src/scenes/home/home.js
+++ src/scenes/home/home.js
@@ -14,6 +14,7 @@ import Header from './header/header';
 import Landing from './landing/landing';
 import Footer from './footer/footer';
 import MentorRequest from './mentorRequest/mentorRequest';
+import SquadsNew from './squads/squadsNew/squadsNew';
 
 class Home extends Component {
   constructor(props) {
--- src/scenes/home/squads/squadsNew/squadsNew.js
+++ src/scenes/home/squads/squadsNew/squadsNew.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import Form from 'shared/components/form/form';
-import FormButton from 'shared/components/form/formButton/formButton';
-import FormInput from 'shared/components/form/formInput/formInput';
-import FormSelect from 'shared/components/form/formSelect/formSelect';
-import * as ApiHelpers from 'shared/utils/apiHelper';
+// import { Redirect } from 'react-router-dom';
+// import Form from 'shared/components/form/form';
+// import FormButton from 'shared/components/form/formButton/formButton';
+// import FormInput from 'shared/components/form/formInput/formInput';
+// import FormSelect from 'shared/components/form/formSelect/formSelect';
+// import * as ApiHelpers from 'shared/utils/apiHelper';
 import Section from 'shared/components/section/section';
-import styles from './squadsNew.css';
+// import styles from './squadsNew.css';
 
 class SquadsNew extends Component {
-
   render() {
     return (
-      <Section title='Create a New Squad'>
-      </ Section>
-      );
+      <Section title="Create a New Squad" />
+    );
   }
 }
-
 export default SquadsNew;
