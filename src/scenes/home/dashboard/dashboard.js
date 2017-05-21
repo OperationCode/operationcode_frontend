87a40ac2fd801c1d25551dae7d480fd7361c667e
Brandon Lawrence <brandon.lawrence@customink.com>
2017-05-21 10:23:40 -0400

    update dashboard links

--- src/scenes/home/dashboard/dashboard.js
+++ src/scenes/home/dashboard/dashboard.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react';
-import LinkButton from 'shared/components/linkButton/linkButton';
+import Cookies from 'universal-cookie';
+import { Link } from 'react-router-dom';
 import Section from 'shared/components/section/section';
+import styles from './dashboard.css';
 
 class Dashboard extends Component {
+
+  state = {
+    mentor: null
+  }
+
+  componentWillMount() {
+    const cookies = new Cookies();
+    this.setState({ mentor: cookies.get('mentor') });
+  }
+
+  renderLinks = () => {
+    const { mentor } = this.state;
+    return mentor === 'true'
+    ? (
+      <div>
+        <h1 className={styles.link}>
+          <Link to="/mentor/requests">View Requests</Link>
+        </h1>
+        <h1 className={styles.link}>
+          <Link to="/squads/new-squad">Create a Squad</Link>
+        </h1>
+        <h1 className={styles.link}>
+          <Link to="/squads">View Squads</Link>
+        </h1>
+        <h1 className={styles.link}>
+          <Link to="/mentors">View Mentors</Link>
+        </h1>
+      </div>
+    ) : (
+      <div>
+        <h1 className={styles.link}>
+          <Link to="/mentor-request">Request Help</Link>
+        </h1>
+        <h1 className={styles.link}>
+          <Link to="/squads">Join a Squad</Link>
+        </h1>
+        <h1 className={styles.link}>
+          <Link to="/mentors">View Mentors</Link>
+        </h1>
+      </div>
+    );
+  }
   render() {
     return (
-
       <Section title="What would you like to do?" theme="white">
-        <LinkButton text="Request Help" theme="blue" link="/mentor-request" />
-        <LinkButton text="Join a Squad" theme="blue" link="/squads" />
-        <LinkButton text="View Mentors" theme="blue" link="/mentors" />
+        {this.renderLinks()}
       </Section>
     );
   }
