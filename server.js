b270fb67e7f110dd537ad8764b75870cde265bbb
Alexander Spence <aspence@smashglass.com>
2017-05-30 16:20:08 -0500

    forward http requests to https

--- server.js
+++ server.js
@@ -4,27 +4,16 @@ const path = require('path');
 
 const app = express();
 
-const FALLBACK_PORT = 4000;
-
-// Redirect all HTTP traffic to HTTPS
-function ensureSecure(req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    return next();
+app.use((req, res, next) => {
+  if (!req.secure) {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
   }
-  let redirectUrl = `https://${req.hostname}`;
-  let port = parseInt(process.env.PORT, 10);
-  if (port !== 443) {
-    if (!port) port = FALLBACK_PORT;
-    redirectUrl = `${redirectUrl}:${port}`;
-  }
-  redirectUrl = redirectUrl.concat(req.url);
-  return res.redirect(redirectUrl);
-}
-app.all('*', ensureSecure);
+  return next();
+});
 
 app.use(express.static('./build'));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-app.listen(process.env.PORT || FALLBACK_PORT);
+app.listen(process.env.PORT);
