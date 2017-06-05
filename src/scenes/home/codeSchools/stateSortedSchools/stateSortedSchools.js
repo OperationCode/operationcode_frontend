7f744d7f10c01ce9fd039264c5fada08cc72d0a3
Evan Cooper <evanncooper@gmail.com>
2017-06-04 21:18:52 -0400

    Add form input event handlers and state handlers to stateSortedSchools

--- src/scenes/home/codeSchools/stateSortedSchools/stateSortedSchools.js
+++ src/scenes/home/codeSchools/stateSortedSchools/stateSortedSchools.js
@@ -3,13 +3,30 @@ import Section from 'shared/components/section/section';
 import FormInput from 'shared/components/form/formInput/formInput';
 // import styles from './stateSortedSchools.css';
 
+const endpoint = 'https://raw.githubusercontent.com/OperationCode/operationcode_frontend/code-schools-cooper-kyle/src/scenes/home/codeSchools/schools.json';
+const gettingSchoolData = fetch(endpoint)
+  .then(response => response.json());
+
+
 class StateSortedSchools extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      by_state: {}
+      query: null,
+      stateList: null,
+      by_state: null
     };
+
+    gettingSchoolData.then(data =>
+      this.setState({ by_state: data.by_state })
+    );
+  }
+
+  onSearchChange = (value) => {
+    const regex = new RegExp(value, 'gi');
+    const arr = this.state.by_state.match(regex);
+    this.setState({ stateList: arr });
+      // this.setState({ query: value });
   }
 
   render() {
@@ -22,6 +39,7 @@ class StateSortedSchools extends Component {
       >
         <FormInput
           placeholder="Start typing a state..."
+          onChange={this.onSearchChange}
         />
       </Section>
     );
