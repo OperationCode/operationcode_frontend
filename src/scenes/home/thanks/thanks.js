da4b70eb3645e6b5ff19cca7f8cd7dab8eb2ee87
Alexander Spence <aspence@smashglass.com>
2017-06-13 17:36:38 -0500

    Removed Trademarks Fixes #139

--- src/scenes/home/thanks/thanks.js
+++ src/scenes/home/thanks/thanks.js
@@ -5,7 +5,7 @@ class Thanks extends Component {
   render() {
     return (
       <Section title="Thanks for joining">
-        <span>Thanks for joining operation code™.  Please check your email for orders.</span>
+        <span>Thanks for joining operation code.  Please check your email for orders.</span>
       </Section>
     );
   }
