38eeda33193f12c4d70fe0ec53a5940310f88581
RickR <rrein@akamai.com>
2017-06-14 09:43:52 -0600

    Only check for production mode for GA. Fixes #143

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-    if (process.env.NODE_ENV === 'production' && process.env.GA_TRACKER_ID) {
-      ReactGA.initialize(process.env.GA_TRACKER_ID);
+    if (process.env.NODE_ENV === 'production') {
+      ReactGA.initialize('UA-75642413-1', { debug: false });
       this.history.listen((location) => {
         ReactGA.set({ page: location.pathname });
         ReactGA.pageview(location.pathname);
