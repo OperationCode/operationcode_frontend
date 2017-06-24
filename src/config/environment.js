4ca0a224142ed82daf2cc4b8184fc038ffcf1ac9
Alexander Spence <aspence@smashglass.com>
2017-06-24 11:00:39 -0500

    Docker update

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,8 +1,8 @@
 const config = {
-  backendUrl: 'http://localhost:3000/api/v1',
-  idmeClientId: '6d781bfd42506613a0fe4ad4123aaf6d',
-  host: 'http://localhost:4000',
-  idmeOAuthUrl: 'http://localhost:4001/oauth/authorize'
+  backendUrl: process.env.OC_BACKEND_URL || 'http://localhost:3000/api/v1',
+  host: process.env.OC_HOST || 'http://localhost:4000',
+  idmeClientId: process.env.OC_IDME_CLIENT_ID || '6d781bfd42506613a0fe4ad4123aaf6d',
+  idmeOAuthUrl: process.env.OC_IDME_AUTH_URL || 'http://localhost:4001/oauth/authorize'
 };
 
 export default config;
