71a1b842f9a3e68c36cbeeb2d6a9c7fb0575ee70
Alexander Spence <aspence@smashglass.com>
2017-06-25 02:19:00 -0500

    restore redirect in server.js

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
-
 const path = require('path');
 
 const app = express();
 
 app.enable('trust proxy');
 
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  return next();
+});
+
 app.use(express.static('./build'));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-app.listen(4000);
+app.listen(process.env.PORT);
