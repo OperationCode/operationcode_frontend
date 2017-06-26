986ca242318df1ed631f100732fd4c8a44ff9ffa
Evan Cooper <evanncooper@gmail.com>
2017-06-26 14:40:26 -0400

    Add a team scene and relevant starter files

---
+++ src/scenes/home/team/team.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Section from 'shared/components/section/section';
+import styles from './team.css';
+
+import React from 'react';
+
+export default class Team extends React.Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
