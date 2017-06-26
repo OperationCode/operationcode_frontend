3759680cfef02219b1de8d0a99c45e72eda1758e
Evan Cooper <evanncooper@gmail.com>
2017-06-26 16:32:16 -0400

    Add service seals to our front-end image repo and add them to the team page

--- src/scenes/home/team/team.js
+++ src/scenes/home/team/team.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 import Section from 'shared/components/section/section';
-// import styles from './team.css';
+import armySeal from 'images/serviceSeals/Army.png';
+import navySeal from 'images/serviceSeals/Navy.png';
+import marineSeal from 'images/serviceSeals/USMC.png';
+import coastGuardSeal from 'images/serviceSeals/USCG.png';
+import airForceSeal from 'images/serviceSeals/AirForce.png';
+import armyReserveSeal from 'images/serviceSeals/ArmyReserve.png';
+import navyReserveSeal from 'images/serviceSeals/NavyReserve.png';
+import styles from './team.css';
 
 class Team extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.wrapper}>
         <h1>
           This is the header section
         </h1>
-        <Section
-          title="Mission"
-        />
+        <Section title="Mission" theme="white">
+          <p className={styles.paragraph}>Operation code is leading the way to expand opportunities for military veterans and their families to learn new skills and build a career in the fast-growing technology sector. Our team’s mission, led by veterans and other dedicated volunteers who are passionate about technology, is to hep open doors for our diverse membership through unique program offerings, such as our Software Mentor Program, Conference Scholarships, and Employment Services - all made possible by individual donations and corporate partnerships.
+          </p>
+
+          <h2>Who We Serve</h2>
+          <p className={styles.paragraph}>
+         Past and present Army, Navy, Marines, Air Force, and Coast Guard service members, veterans, military spouses, and family members are welcome to apply to any of our programs. Through our online community, we work with 2000+ members around the world.</p>
+          <div className={styles.serviceSealWrapper}>
+            <img className={styles.serviceSeal} src={armySeal} alt="Army Seal" />
+            <img className={styles.serviceSeal} src={navySeal} alt="Army Seal" />
+            <img className={styles.serviceSeal} src={marineSeal} alt="Army Seal" />
+            <img className={styles.serviceSeal} src={coastGuardSeal} alt="Army Seal" />
+            <img className={styles.serviceSeal} src={airForceSeal} alt="Army Seal" />
+            <img className={styles.serviceSeal} src={armyReserveSeal} alt="Army Seal" />
+            <img className={styles.serviceSeal} src={navyReserveSeal} alt="Army Seal" />
+          </div>
+        </Section>
       </div>
     );
   }
