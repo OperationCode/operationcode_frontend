d95f43d57f80b3ffa329c951d9dad8323bd4ab9e
Evan Cooper <evanncooper@gmail.com>
2017-06-27 14:36:00 -0400

    Style the paragraphs and headline text for FAQ

--- src/scenes/home/faq/faq.js
+++ src/scenes/home/faq/faq.js
@@ -7,9 +7,25 @@ class FAQ extends Component {
     return (
       <div>
         <Section title="Frequently Asked Questions" theme="white">
+          <p className={styles.paragraph}>
+            If you have any additional questions, please feel free to e-mail contact@operationcode.org
+          </p>
+          <span className={styles.headline}>General Questions</span>
           <div className={styles.container}>
             <div className={styles.accordion}>
-              The questions go here
+              Bacon ipsum dolor amet tenderloin alcatra pork belly rump spare ribs short loin brisket ribeye picanha. Biltong pancetta prosciutto short loin brisket jerky, jowl beef leberkas short ribs ham hock pig. Ribeye tongue tri-tip ground round cupim. Jowl shankle chuck rump turducken.
+            </div>
+          </div>
+          <span className={styles.headline}>Donation Questions</span>
+          <div className={styles.container}>
+            <div className={styles.accordion}>
+              Bacon ipsum dolor amet tenderloin alcatra pork belly rump spare ribs short loin brisket ribeye picanha. Biltong pancetta prosciutto short loin brisket jerky, jowl beef leberkas short ribs ham hock pig. Ribeye tongue tri-tip ground round cupim. Jowl shankle chuck rump turducken.
+            </div>
+          </div>
+          <span className={styles.headline}>Volunteer Questions</span>
+          <div className={styles.container}>
+            <div className={styles.accordion}>
+              Bacon ipsum dolor amet tenderloin alcatra pork belly rump spare ribs short loin brisket ribeye picanha. Biltong pancetta prosciutto short loin brisket jerky, jowl beef leberkas short ribs ham hock pig. Ribeye tongue tri-tip ground round cupim. Jowl shankle chuck rump turducken.
             </div>
           </div>
         </Section>
