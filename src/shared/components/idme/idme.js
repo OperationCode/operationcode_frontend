c47782f5d198bc74130670e29fcd29f142ccd0a1
Alexander Spence <aspence@smashglass.com>
2017-06-27 10:00:41 -0500

    idme update.  Use official idme troop image.

--- src/shared/components/idme/idme.js
+++ src/shared/components/idme/idme.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import Button from 'shared/components/button/button';
 import config from 'config/environment';
+import idmeImage from 'images/IdmeTroop.png';
 import styles from './idme.css';
 
 class Idme extends Component {
   componentWillMount() {
-    const script = document.createElement('script');
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js';
-    script.async = true;
-    const script1 = document.createElement('script');
-    script1.src = 'https://s3.amazonaws.com/idme/developer/idme-buttons-2.0.1/assets/js/idme-modal.min.js';
-    script1.async = true;
-    document.body.appendChild(script, script1);
+
   }
   idMe = () => {
     window.open(`${config.idmeOAuthUrl}?client_id=${config.idmeClientId}&redirect_uri=${config.host}/profile/verify&response_type=token&scope=military&display=popup', '', 'scrollbars=yes,menubar=no,status=no,location=no,toolbar=no,width=750,height=780,top=200,left=200`);
@@ -20,10 +14,10 @@ class Idme extends Component {
   render() {
     return (
       <div className={styles.wrapper}>
-        <span className={styles.auth}>If you are interested in scholarships and / or hardware loans, please authenticate yourself using the button below.</span>
-        <Button className={styles.authbtn} onClick={this.idMe}>
-          ID.Me Authentication
-        </Button>
+        <span className={styles.auth}>If you are interested in scholarships and / or hardware loans, verify your veteran status with Id.Me below.</span>
+        <span className={styles.authbtn} role="link" onClick={this.idMe} tabIndex={0} >
+          <img className={styles.authImage} src={idmeImage} alt="Verify your status with Id.Me" />
+        </span>
       </div>
     );
   }
