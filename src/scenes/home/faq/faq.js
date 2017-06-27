7d9143c1f5daa4f11848e6e5f834b9cdc68808db
Evan Cooper <evanncooper@gmail.com>
2017-06-27 15:29:33 -0400

    Render question components for each question section, Add LinkButton and Link

--- src/scenes/home/faq/faq.js
+++ src/scenes/home/faq/faq.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import LinkButton from 'shared/components/linkButton/linkButton';
+import { Link } from 'react-router-dom';
 import Section from 'shared/components/section/section';
 import Question from './question/question';
 import Data from './questions.json';
@@ -6,28 +8,53 @@ import styles from './faq.css';
 
 class FAQ extends Component {
   render() {
+    const generalQuestions = Data.general
+      .map(question =>
+        (
+          <Question
+            q={question.question}
+            a={question.answer}
+          />
+        )
+    );
+    const donationQuestions = Data.donation
+      .map(question =>
+        (
+          <Question
+            q={question.question}
+            a={question.answer}
+          />
+        )
+    );
+    const volunteerQuestions = Data.volunteer
+      .map(question =>
+        (
+          <Question
+            q={question.question}
+            a={question.answer}
+          />
+        )
+    );
+
     return (
       <div>
         <Section title="Frequently Asked Questions" theme="white">
-          <p className={styles.paragraph}>
-            If you have any additional questions, please feel free to e-mail contact@operationcode.org
-          </p>
           <span className={styles.headline}>General Questions</span>
           <div className={styles.container}>
             <div className={styles.accordion}>
-              Bacon ipsum dolor amet tenderloin alcatra pork belly rump spare ribs short loin brisket ribeye picanha. Biltong pancetta prosciutto short loin brisket jerky, jowl beef leberkas short ribs ham hock pig. Ribeye tongue tri-tip ground round cupim. Jowl shankle chuck rump turducken.
+              {generalQuestions}
             </div>
           </div>
           <span className={styles.headline}>Donation Questions</span>
           <div className={styles.container}>
             <div className={styles.accordion}>
-              Bacon ipsum dolor amet tenderloin alcatra pork belly rump spare ribs short loin brisket ribeye picanha. Biltong pancetta prosciutto short loin brisket jerky, jowl beef leberkas short ribs ham hock pig. Ribeye tongue tri-tip ground round cupim. Jowl shankle chuck rump turducken.
+              {donationQuestions}
             </div>
           </div>
           <span className={styles.headline}>Volunteer Questions</span>
           <div className={styles.container}>
             <div className={styles.accordion}>
-              Bacon ipsum dolor amet tenderloin alcatra pork belly rump spare ribs short loin brisket ribeye picanha. Biltong pancetta prosciutto short loin brisket jerky, jowl beef leberkas short ribs ham hock pig. Ribeye tongue tri-tip ground round cupim. Jowl shankle chuck rump turducken.
+              {volunteerQuestions}
             </div>
           </div>
         </Section>
