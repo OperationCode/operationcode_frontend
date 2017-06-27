67c9126e9686b3c002b6f2d18b1a3b7a47cc4ebe
Evan Cooper <evanncooper@gmail.com>
2017-06-27 16:35:25 -0400

    Add conrad to board, split out team into a different section, include json in team section, and render a teamCard per person

--- src/scenes/home/team/team.js
+++ src/scenes/home/team/team.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Section from 'shared/components/section/section';
-// import TeamCard from 'shared/components/teamCard/teamCard';
+import TeamCard from 'shared/components/teamCard/teamCard';
 import StaffCard from 'shared/components/staffCard/staffCard';
 import armySeal from 'images/serviceSeals/Army.png';
 import navySeal from 'images/serviceSeals/Navy.png';
@@ -12,16 +12,36 @@ import airForceReserveSeal from 'images/serviceSeals/AirForceReserve.png';
 import armyReserveSeal from 'images/serviceSeals/ArmyReserve.png';
 import navyReserveSeal from 'images/serviceSeals/NavyReserve.png';
 import styles from './team.css';
+import Data from './team.json';
 
 class Team extends Component {
   render() {
     const founder = {
       name: 'David Molina',
-      role: 'President / Founder',
+      role: 'Founder / Executive Director',
       src: 'https://pbs.twimg.com/profile_images/875514479202639873/vFr09yW4_400x400.jpg',
+      alt: 'David Molina',
       twitter: '@davidcmolina',
       email: 'david@operationcode.org'
     };
+    const coo = {
+      name: 'Conrad Holloman',
+      role: 'Chief of Staff / COO',
+      src: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAP6AAAAJDk2MTJkMDc3LTdiZDYtNGNkZi1hOTgxLWE3YmI0OWFjYzQ4Nw.jpg',
+      alt: 'Conrad Holloman',
+      twitter: '@hollomancer',
+      email: 'conrad@operationcode.org'
+    };
+    const team = Data
+      .map(person =>
+        (
+          <TeamCard
+            name={person.name}
+            role={person.role}
+            key={person.name}
+          />
+        )
+      );
     return (
       <div className={styles.wrapper}>
         <div className={styles.titleQuote}>
@@ -50,22 +70,22 @@ class Team extends Component {
             <img className={styles.serviceSeal} src={navyReserveSeal} alt="Army Seal" />
           </div>
         </Section>
-        <Section title="Our Team" theme="gray">
+        <Section title="Our Board" theme="gray">
+          <p className={styles.paragraph}>
+            Our fearless volunteer leaders. No salaries here.
+          </p>
+          <div className={styles.team}>
+            <StaffCard name={founder.name} role={founder.role} src={founder.src} twitter={founder.twitter} email={founder.email} alt={founder.alt} />
+            <StaffCard name={coo.name} role={coo.role} src={coo.src} twitter={coo.twitter} email={coo.email} alt={coo.alt} />
+          </div>
+        </Section>
+        <Section title="Our Team" theme="gray" id="long">
           <p className={styles.paragraph}>
             Our all volunteer staff are dedicated individuals who come from a wide variety of backgrounds, including U.S Military Veterans, civilians, those with tech backgrounds, and those who have skills outside of web development.
           </p>
-          <span className={styles.headline}>The Board</span>
           <div className={styles.team}>
-            <StaffCard name={founder.name} role={founder.role} src={founder.src} twitter={founder.twitter} email={founder.email} />
-            <StaffCard name={founder.name} role={founder.role} src={founder.src} twitter={founder.twitter} email={founder.email} />
-            <StaffCard name={founder.name} role={founder.role} src={founder.src} twitter={founder.twitter} email={founder.email} />
-            <StaffCard name={founder.name} role={founder.role} src={founder.src} twitter={founder.twitter} email={founder.email} />
+            {team}
           </div>
-          <span className={styles.headline}>The Team</span>
-          <div className={styles.team}>
-            The Team
-          </div>
-
         </Section>
       </div>
     );
