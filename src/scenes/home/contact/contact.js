34b38aa817832df93203aa43663170500c9fb9ff
Evan Cooper <evanncooper@gmail.com>
2017-06-29 14:55:25 -0400

    Further styling for the contact component

--- src/scenes/home/contact/contact.js
+++ src/scenes/home/contact/contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Section from 'shared/components/section/section';
 import emailIcon from 'images/icons/Email-Icon.svg';
+import locationIcon from 'images/icons/Location-Icon.svg';
 import styles from './contact.css';
 
 
@@ -10,15 +11,19 @@ class Contact extends Component {
       <div>
         <Section title="Contact Us" theme="white">
           <p className={styles.paragraph}>
-            We keep our organization intentionally decentralized so every penny of our operating budget goes directly to our programming. If you do need to contact us directly, feel free to join our <a href="https://operation-code.slack.com">Slack</a> or use one of the methods below. 
+            We keep our organization intentionally decentralized so every penny of our operating budget goes directly to our programming. If you do need to contact us directly, feel free to join our <a href="https://operation-code.slack.com">Slack</a> or use one of the methods below.
           </p>
           <div className={styles.wrapper}>
             <div className={styles.left}>
               <img src={emailIcon} alt="Email Icon" />
-              <a href="staff@operationcode.org">Staff@operationcode.org</a>  
+              <a href="staff@operationcode.org"><p className={styles.paragraph}>Staff@operationcode.org</p></a>
             </div>
             <div className={styles.right}>
-            </div>   
+              <img src={locationIcon} alt="Location Icon" />
+              <p className={styles.paragraph}>
+                Physical address <br /> Operation Code  <br />1220 SW 11th Avenue, Suite 437  <br /> Portland, OR 97205
+              </p>
+            </div>
           </div>
         </Section>
       </div>
