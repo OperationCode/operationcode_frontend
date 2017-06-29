ff1d93c9a0bf5e7f9968e1ca6c93a7b87ef76f1e
Evan Cooper <evanncooper@gmail.com>
2017-06-29 17:03:12 -0400

    Remove icons and add final styles for contact page

--- src/scenes/home/contact/contact.js
+++ src/scenes/home/contact/contact.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
 import Section from 'shared/components/section/section';
-import emailIcon from 'images/icons/Email-Icon.svg';
-import locationIcon from 'images/icons/Location-Icon.svg';
 import styles from './contact.css';
 
 
 class Contact extends Component {
   render() {
     return (
-      <div>
-        <Section title="Contact Us" theme="white">
-          <p className={styles.paragraph}>
-            We keep our organization intentionally decentralized so every penny of our operating budget goes directly to our programming. If you do need to contact us directly, feel free to join our <a href="https://operation-code.slack.com">Slack</a> or use one of the methods below.
-          </p>
-          <div className={styles.wrapper}>
-            <div className={styles.left}>
-              <img src={emailIcon} alt="Email Icon" />
-              <a href="staff@operationcode.org"><p className={styles.paragraph}>Staff@operationcode.org</p></a>
-            </div>
-            <div className={styles.right}>
-              <img src={locationIcon} alt="Location Icon" />
-              <p className={styles.paragraph}>
-                Physical address <br /> Operation Code  <br />1220 SW 11th Avenue, Suite 437  <br /> Portland, OR 97205
-              </p>
-            </div>
+      <div className={styles.wrapper}>
+        <h3 className={styles.title}>
+          Contact Us
+        </h3>
+        <p className={styles.paragraph}>
+          We are a decentralized community of hard-working volunteers. We love feedback, comments, and suggestions. <br />Find us on Slack, send an e-mail, or even mail us a letter.
+        </p>
+        <br />
+        <Section theme="gray">
+          <div className={styles.section}>
+            <h3 className={styles.headline}>
+              Email Inquiries:
+            </h3>
+            <br />
+            <span className={styles.paragraph}>
+              <a href="mailto:staff@operationcode.org">Staff@operationcode.org</a>
+            </span>
+          </div>
+          <hr className={styles.line} />
+          <div className={styles.section}>
+            <h3 className={styles.headline}>
+              Slack:
+            </h3>
+            <br />
+            <span className={styles.paragraph}>
+              <a href="https://operation-code.slack.com">Slack Chat</a>
+            </span>
+          </div>
+          <hr className={styles.line} />
+          <div className={styles.section}>
+            <h3 className={styles.headline}>
+              Mailing Address:
+            </h3>
+            <p className={styles.paragraph}>
+              Operation Code <br /> 1220 SW 11th Avenue <br /> Suite 437  <br /> Portland, OR 97205
+            </p>
           </div>
         </Section>
       </div>
