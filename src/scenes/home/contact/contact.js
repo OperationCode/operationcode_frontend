e43236f1b9f97b31b81addd530b85ac472255a87
Kyle Holmberg <kylemh.email@gmail.com>
2017-07-05 01:05:08 -0700

    Implement requested changes and condense UI

--- src/scenes/home/contact/contact.js
+++ src/scenes/home/contact/contact.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { FaEnvelope, FaSlack, FaHome } from 'react-icons/lib/fa';
 import Section from 'shared/components/section/section';
 import styles from './contact.css';
 
@@ -7,41 +9,69 @@ class Contact extends Component {
   render() {
     return (
       <div className={styles.wrapper}>
-        <h3 className={styles.title}>
-          Contact Us
-        </h3>
-        <p className={styles.paragraph}>
-          We are a decentralized community of hard-working volunteers. We love feedback, comments, and suggestions. <br />Find us on Slack, send an e-mail, or even mail us a letter.
-        </p>
+        <Section
+          title="Contact Us"
+        >
+          <p className={styles.introParagraph}>
+            We are a decentralized community of hard-working volunteers, and we love hearing feedback, comments, and suggestions. Feel free to send us an e-mail, or even mail a letter.
+            <br />
+            <br />
+            The best way to reach our staff and our members is by <Link to="join">joining Operation Code</Link> to receive an invite to our Slack team..
+          </p>
+        </Section>
+
         <br />
-        <Section theme="gray">
-          <div className={styles.section}>
-            <h3 className={styles.headline}>
-              Email Inquiries:
-            </h3>
-            <br />
-            <span className={styles.paragraph}>
-              <a href="mailto:staff@operationcode.org">Staff@operationcode.org</a>
-            </span>
-          </div>
-          <hr className={styles.line} />
-          <div className={styles.section}>
-            <h3 className={styles.headline}>
-              Slack:
-            </h3>
-            <br />
-            <span className={styles.paragraph}>
-              <a href="https://operation-code.slack.com">Slack Chat</a>
-            </span>
-          </div>
-          <hr className={styles.line} />
-          <div className={styles.section}>
-            <h3 className={styles.headline}>
-              Mailing Address:
-            </h3>
-            <p className={styles.paragraph}>
-              Operation Code <br /> 1220 SW 11th Avenue <br /> Suite 437  <br /> Portland, OR 97205
-            </p>
+
+        <Section
+          theme="white"
+          headingLines={false}
+        >
+          <div className={styles.flexContainer}>
+            <div className={styles.columns}>
+              <h3>Email</h3>
+              <a href="mailto:staff@operationcode.org">
+                <FaEnvelope
+                  className={styles.mailIcon}
+                  size={200}
+                />
+              </a>
+              <p>
+                <a href="mailto:staff@operationcode.org">
+                  staff@operationcode.org
+                </a>
+              </p>
+            </div>
+
+            <div className={styles.columns}>
+              <h3>Slack Team</h3>
+              <a href="https://operation-code.slack.com">
+                <FaSlack
+                  className={styles.slackIcon}
+                  size={170}
+                />
+              </a>
+              <p>
+                <a href="https://operation-code.slack.com">
+                  https://operation-code.slack.com
+                </a>
+              </p>
+            </div>
+
+            <div className={styles.columns}>
+              <h3>Mailing Address</h3>
+              <FaHome
+                size={217}
+              />
+              <p className={styles.address}>
+                Operation Code
+                <br />
+                1220 SW 11th Avenue
+                <br />
+                Suite 437
+                <br />
+                Portland, OR 97205
+              </p>
+            </div>
           </div>
         </Section>
       </div>
