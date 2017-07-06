20efac7ae0533a449f45ee0d8637e3e8b59b839e
Evan Cooper <evanncooper@gmail.com>
2017-07-06 15:33:55 -0400

    Create history scene and basic history component

---
+++ src/scenes/home/history/history.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Section from 'shared/components/section/section';
+import styles from './history.css';
+
+class History extends Component {
+  render() {
+    return (
+      <Section title="Our History" theme="white">
+        The history slider will go here
+      </Section>
+    );
+  }
+}
+
+export default History;
