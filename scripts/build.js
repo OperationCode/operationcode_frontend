00fbdbeb3d8da94f90b562f856bd66c6125e0683
RickR <rrein@akamai.com>
2017-07-08 17:54:30 -0400

    Set build vars for production. Fixes #228

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,10 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
+process.env.OC_BACKEND_URL = 'https://api.operationcode.org/api/v1';
+process.env.IDMECLIENTID = '6d781bfd42506613a0fe4ad4123aaf6d';
+process.env.HOST = 'https://www.operationcode.org',
+process.env.IDMEOAUTHURL = 'https://api.id.me/oauth/authorize'
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
