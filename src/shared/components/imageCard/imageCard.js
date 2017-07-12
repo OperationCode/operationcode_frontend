eef5a3f178cd46d2ad4f8548ac1ab9a114b35ce1
Kyle Holmberg <kylemh.email@gmail.com>
2017-07-12 15:08:40 -0700

    Fix ImageCard stylings to be perfectly responsive

--- src/shared/components/imageCard/imageCard.js
+++ src/shared/components/imageCard/imageCard.js
@@ -6,14 +6,13 @@ import styles from './imageCard.css';
 class ImageCard extends Component {
   render() {
     return (
-      <div className={styles.imageCard} >
-        <img className={styles.image} src={this.props.image} alt={this.props.title} />
-        <div className={styles.cardContainer}>
-          <div className={styles.cardText}>
-            <h2>{this.props.title}</h2>
-            <p>{this.props.cardText}</p>
-            {this.props.link && <LinkButton text={this.props.buttonText} link={this.props.link} />}
-          </div>
+      <div className={styles.imageCard}>
+        <img className={styles.cardImage} src={this.props.image} alt={this.props.title} />
+
+        <div className={styles.cardText}>
+          <h6>{this.props.title}</h6>
+          <p>{this.props.cardText}</p>
+          {this.props.link && <LinkButton text={this.props.buttonText} link={this.props.link} />}
         </div>
       </div>
     );
