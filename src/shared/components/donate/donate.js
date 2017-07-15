a8182cbfb30f6f4590d06ebfd2624ecf585045f0
Kyle Holmberg <kylemh.email@gmail.com>
2017-07-15 15:46:39 -0700

    Convert <Donate> to newer React component syntax

--- src/shared/components/donate/donate.js
+++ src/shared/components/donate/donate.js
@@ -1,32 +1,32 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import Section from 'shared/components/section/section';
 import LinkButton from 'shared/components/linkButton/linkButton';
 import styles from './donate.css';
 
-const Donate = (props) => {
-  const { ...otherProps } = props;
-  return (
-    <Section
-      title="Donate" headingLines={false} {...otherProps}
-      className={styles.donateSection} headingTheme="white"
-    >
-      <div className={styles.donate} >
-        <p>
-          As a 501(c)(3) veteran-led nonprofit organization,
-          our programs and services are maintained
-          through the efforts of our volunteer staff.
-          Your financial support allows us to continue helping
-          the military community learn software development,
-          enter the tech industry, and code the future.
-        </p>
-        <p>Thank you for supporting our mission!</p>
-        <LinkButton text="Donate Now" link="https://donorbox.org/operationcode" />
-      </div>
-    </Section>
-  );
-};
-
-Donate.propTypes = {};
+class Donate extends Component {
+  render() {
+    return (
+      <Section
+        title="Donate"
+        headingLines={false}
+        className={styles.donateSection}
+        headingTheme="white"
+      >
+        <div className={styles.donate} >
+          <p>
+            As a 501(c)(3) veteran-led nonprofit organization,
+            our programs and services are maintained
+            through the efforts of our volunteer staff.
+            Your financial support allows us to continue helping
+            the military community learn software development,
+            enter the tech industry, and code the future.
+          </p>
+          <p>Thank you for supporting our mission!</p>
+          <LinkButton text="Donate Now" link="https://donorbox.org/operationcode" />
+        </div>
+      </Section>
+    );
+  }
+}
 
 export default Donate;
