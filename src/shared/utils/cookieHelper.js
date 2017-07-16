73814f6e510e8815b67b3451fd99b22825ee389f
RickR <rrein@akamai.com>
2017-07-15 22:23:32 -0400

    Adds AuthenticatedRoute to protect pages. Bolsters JWT validation
    Fixes #267
    Fixes #262

--- src/shared/utils/cookieHelper.js
+++ src/shared/utils/cookieHelper.js
@@ -1,8 +1,16 @@
 import Cookies from 'universal-cookie';
+import jwt_decode from 'jwt-decode'; // eslint-disable-line camelcase
+
+const cookieDomain = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return 'localhost';
+  }
+  return 'operationcode.org';
+};
 
 export const setUserAuthCookie = ({ token, user }) => {
   const cookies = new Cookies();
-  cookies.set('token', token, { path: '/', domain: 'operationcode.org' });
+  cookies.set('token', token, { path: '/', domain: cookieDomain() });
   cookies.set('firstName', user.first_name, { path: '/' });
   cookies.set('lastName', user.last_name, { path: '/' });
   cookies.set('slackName', user.slack_name, { path: '/' });
@@ -25,12 +33,26 @@ export const isMentor = () => {
   return cookies.get('mentor');
 };
 
+const validToken = (token) => {
+  if (token === undefined) { return false; }
+
+  const jwt = jwt_decode(token);
+  const currentTime = new Date().getTime() / 1000;
+
+  // TODO we could validate the signature as well (issue#281)
+
+  if (currentTime > jwt.exp) { return false; }
+
+  return true;
+};
+
 export const getUserStatus = () => {
   const cookies = new Cookies();
 
   return {
     mentor: cookies.get('mentor') === 'true',
-    signedIn: cookies.get('token') !== undefined,
+    signedIn: validToken(cookies.get('token')),
     verified: cookies.get('verified') === 'true'
   };
 };
+
