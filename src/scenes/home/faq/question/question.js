c99831b6dfca046626a6b01085abee91f5ab5d63
Seth Bergman <seth.atxwebs@gmail.com>
2017-07-19 17:18:05 -0500

    ESLINT is happy

--- src/scenes/home/faq/question/question.js
+++ src/scenes/home/faq/question/question.js
@@ -8,10 +8,9 @@ class Question extends Component {
       <div className={styles.accordionSingle}>
         <input className={styles.accordionSingleHidden} type="checkbox" id={this.props.q} />
         <label className={styles.accordionSingleQuestion} htmlFor={this.props.q}> {this.props.q}</label>
-        {   // eslint-disable-next-line react/no-danger react/prop-types: 0) }
-            // eslint-disable-next-line
-          <p className={styles.accordionSingleAnswer} dangerouslySetInnerHTML={{ __html: this.props.a }} />
-      }</div>
+        { // eslint-disable-next-line
+          <p className={styles.accordionSingleAnswer} dangerouslySetInnerHTML={{ __html: this.props.a }} />}
+      </div>
     );
   }
 }
