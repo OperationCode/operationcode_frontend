6c4c0a43e6aee451cc255a6f383d17d4755c8b66
Kyle Holmberg <kylemh.email@gmail.com>
2017-07-22 12:06:34 -0700

    Fix #279 - Contact page design adjustments

--- src/scenes/home/contact/contact.js
+++ src/scenes/home/contact/contact.js
@@ -9,18 +9,18 @@ class Contact extends Component {
     return (
       <div>
         <Section title="Contact Us" >
-          <p>
+          <p className={styles.centerText}>
             We are a decentralized community of hard-working volunteers, and we love hearing feedback, comments, and suggestions!
             <br />
             <br />
-            The best way to reach our staff and our members is by <Link to="join">joining Operation Code</Link> to receive an invite to our Slack team.
-            You can also reach us on via e-mail, physical, or <a href="twitter.com/operation_code">Twitter.</a>
+            The best way to reach our staff and our members is by <Link to="join">joining Operation Code</Link> to receive an invite to our Slack team. You can also reach us via email, physical mail, or via <a href="twitter.com/operation_code">Twitter.</a>
           </p>
         </Section>
 
         <Section theme="white" headingLines={false} >
           <div className={styles.flexContainer}>
-            <div className={styles.columns}>
+            {/* Columns */}
+            <div className={styles.centerText}>
               <h3>Email</h3>
               <a href="mailto:staff@operationcode.org">
                 <FaEnvelope className={styles.mailIcon} size={200} />
@@ -30,7 +30,7 @@ class Contact extends Component {
               </p>
             </div>
 
-            <div className={styles.columns}>
+            <div className={styles.centerText}>
               <h3>Slack Team</h3>
               <a href="https://operation-code.slack.com">
                 <FaSlack className={styles.slackIcon} size={170} />
@@ -40,7 +40,7 @@ class Contact extends Component {
               </p>
             </div>
 
-            <div className={styles.columns}>
+            <div className={styles.centerText}>
               <h3>Mailing Address</h3>
               <FaHome size={217} />
               <p className={styles.address}>
