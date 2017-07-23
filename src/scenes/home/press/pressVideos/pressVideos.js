ea76f0911a095583b4203ca389e3244ef3328cca
Kyle Holmberg <kylemh.email@gmail.com>
2017-07-22 22:38:58 -0700

    Resize iframes for row display on lg screens

--- src/scenes/home/press/pressVideos/pressVideos.js
+++ src/scenes/home/press/pressVideos/pressVideos.js
@@ -4,8 +4,8 @@ import styles from './pressVideos.css';
 function PressVideos() {
   return (
     <div className={styles.videos}>
-      <iframe title="GitHub Universe: Operation Code" src="https://www.youtube.com/embed/xN7yMoe38xc" frameBorder="0" allowFullScreen width="889" height="500" />
-      <iframe title="GitHub Universe: Operation Code" src="https://player.vimeo.com/video/124866675?byline=0&portrait=0" frameBorder="0" allowFullScreen width="889" height="500" />
+      <iframe title="GitHub Universe: Operation Code" src="https://www.youtube.com/embed/xN7yMoe38xc" frameBorder="0" allowFullScreen width="755" height="425" />
+      <iframe title="GitHub Universe: Operation Code" src="https://player.vimeo.com/video/124866675?byline=0&portrait=0" frameBorder="0" allowFullScreen width="755" height="425" />
     </div>
   );
 }
