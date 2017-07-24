35c3a3db07fe58d57c6b4fa09b2c704fd38c2b54
RickR <rrein@akamai.com>
2017-07-24 15:24:51 -0600

    Adds setPassword form. Fixes #336

--- src/config/environment-prod.js
+++ src/config/environment-prod.js
@@ -1,4 +1,5 @@
 const config = {
+  backendHost: 'https://api.operationcode.org/',
   backendUrl: 'https://api.operationcode.org/api/v1',
   idmeClientId: '6d781bfd42506613a0fe4ad4123aaf6d',
   host: 'https://www.operationcode.org',
--- src/scenes/home/resetPassword/resetPassword.js
+++ src/scenes/home/resetPassword/resetPassword.js
@@ -1,58 +1,26 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { Redirect } from 'react-router-dom';
-import Form from 'shared/components/form/form';
-import FormEmail from 'shared/components/form/formEmail/formEmail';
-import FormButton from 'shared/components/form/formButton/formButton';
 import Section from 'shared/components/section/section';
 import SignUpLink from 'shared/components/signUpLink/signUpLink';
-import config from 'config/environment';
+import RequestToken from './requestToken/requestToken';
+import SetPassword from './setPassword/setPassword';
 import styles from './resetPassword.css';
 
+const queryString = require('query-string');
+
 class ResetPassword extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      emailValid: true,
-      error: false,
-      isValid: true,
-      success: false,
-    };
+  state = {
+    resetPasswordToken: null
   }
 
-  onEmailChange = (value, valid) => {
-    this.setState({ email: value, emailValid: valid });
+  componentDidMount = () => {
+    const parsed = queryString.parse(location.search);
+    this.setState({ resetPasswordToken: parsed.reset_password_token });
   }
-
-  handleOnClick = (e) => {
-    e.preventDefault = true;
-    if (this.isFormValid()) {
-      axios.post(`${config.backendUrl}/users/passwords/reset`, {
-        email: this.state.email
-      }).then(() => {
-        this.setState({ success: true, error: null });
-      }).catch(() => {
-        this.setState({ error: 'We were unable to reset the password for this email' });
-      });
-    }
-  }
-
-  isFormValid = () => this.state.emailValid
 
   render() {
     return (
-      <Section className={styles.resetPassword} title="Join Operation Code">
-        <Form className={styles.resetPasswordForm}>
-          <span>
-            {"Forgot your password? No problem! Enter your email below and we'll send you a new one."}
-          </span>
-          <FormEmail id="email" placeholder="Email" onChange={this.onEmailChange} />
-          {this.state.error ? <p className={styles.errorMessage}>{this.state.error}</p> : null }
-          {this.state.success && <Redirect to="/login" />}
-          <FormButton className={styles.joinButton} text="Reset Password" onClick={this.handleOnClick} theme="red" />
-        </Form>
+      <Section className={styles.resetPassword} title="Reset Password">
+        { this.state.resetPasswordToken ? <SetPassword resetPasswordToken={this.state.resetPasswordToken} /> : <RequestToken /> }
         <SignUpLink />
       </Section>
     );
