0065b5fd915a2ac7beccdef21aec68624ddc8382
K MacLeod <ksmacleod99@gmail.com>
2017-07-25 18:59:54 -0400

    Added 'report issues' link
    Added the report issues link.
    I think I did the Github part right...

--- src/scenes/home/footer/footer.js
+++ src/scenes/home/footer/footer.js
@@ -30,7 +30,8 @@ class Footer extends Component {
               <Link to="/faq">FAQ</Link>
               <Link to="/team">Team</Link>
             </div>
-            <div className={styles.blockGroup} >
+            <div className={styles.blockGroup}>
+              <a href="https://github.com/OperationCode/operationcode_frontend/issues/new" target="_blank" rel="noopener noreferrer">Report A Bug</a>
               <a href="https://smile.amazon.com/ch/47-4247572" target="_blank" rel="noopener noreferrer">Amazon Smile</a>
               <Link to="/contact">Contact</Link>
               <a href="https://www.iubenda.com/privacy-policy/8174861" target="_blank" rel="noopener noreferrer">Privacy</a>
