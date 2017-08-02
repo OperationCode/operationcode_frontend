ad4816be06562cce4e1e3ed2632d53cb8cdd5573
RickR <rrein@akamai.com>
2017-08-01 21:07:54 -0400

    Adds discourse section. Fixes #354

--- src/scenes/home/profile/profile.js
+++ src/scenes/home/profile/profile.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Section from 'shared/components/section/section';
 import Idme from 'shared/components/idme/idme';
+import discourseImage from '../../../images/Discourse_logo.png';
 import styles from './profile.css';
 
 class Profile extends Component {
 
   render() {
     return (
-      <Section title="User Profile">
-        {this.props.verified && <h2>Your profile is verified with id.me</h2>}
-        {!this.props.verified && <div className={styles.idmeButton}><Idme /></div>}
-      </Section>
+      <div>
+        <Section title="Operation Code Community">
+          <span className={styles.discourseText}>Visit the Operation Code Community by clicking below.</span>
+          <a href="https://community.operationcode.org">
+            <img className={styles.discourseImage} src={discourseImage} alt="Verify your status with Id.Me" />
+          </a>
+        </Section>
+
+        <Section title="Veteran Status" theme="white">
+          {this.props.verified && <h2>Your profile is verified with id.me</h2>}
+          {!this.props.verified && <div className={styles.idmeButton}><Idme /></div>}
+        </Section>
+      </div>
     );
   }
 }
