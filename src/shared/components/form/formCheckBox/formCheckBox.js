0d7d322519b368c3309bb622fd3fee6622e80a2d
cooperbuilt <evanncooper@gmail.com>
2017-08-04 19:09:41 -0400

    Update checkBox component to take name for data aggregation purposes

--- src/shared/components/form/formCheckBox/formCheckBox.js
+++ src/shared/components/form/formCheckBox/formCheckBox.js
@@ -8,11 +8,13 @@ class FormCheckBox extends Component {
       <div>
         <input
           type="checkbox"
-          id={this.props.name}
-          value={this.props.name}
+          name={this.props.name}
+          id={this.props.value}
+          value={this.props.value}
+          onChange={this.props.onChange}
         />
         <label htmlFor={this.props.name}>
-          {this.props.name}
+          {this.props.value}
         </label>
       </div>
     );
@@ -20,7 +22,15 @@ class FormCheckBox extends Component {
 }
 
 FormCheckBox.propTypes = {
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func
+  // checked: PropTypes.bool
+};
+
+FormCheckBox.defaultProps = {
+  checked: false,
+  onChange: null
 };
 
 export default FormCheckBox;
