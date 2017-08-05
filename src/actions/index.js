0990956cd184276c59b69a4a5b365eb730c0cc0f
Seth Bergman <seth.atxwebs@gmail.com>
2017-08-05 16:19:13 -0500

    Profile page polish and select fix

---
+++ src/actions/index.js
@@ -0,0 +1,45 @@
+import {connect} from 'react-redux';
+import {pushState} from 'redux-router';
+
+export function requireAuthentication(Component) {
+
+    class AuthenticatedComponent extends React.Component {
+
+        componentWillMount () {
+            this.checkAuth(this.props.isAuthenticated);
+        }
+
+        componentWillReceiveProps (nextProps) {
+            this.checkAuth(nextProps.isAuthenticated);
+        }
+
+        checkAuth (isAuthenticated) {
+            if (!isAuthenticated) {
+                let redirectAfterLogin = this.props.location.pathname;
+                this.props
+                    .dispatch(pushState(null, `/login?next=${redirectAfterLogin}`));
+            }
+        }
+
+        render () {
+            return (
+                <div>
+                    {this.props.isAuthenticated === true
+                        ? <Component {...this.props}/>
+                        : null
+                    }
+                </div>
+            )
+
+        }
+    }
+
+    const mapStateToProps = (state) => ({
+        token: state.auth.token,
+        userName: state.auth.userName,
+        isAuthenticated: state.auth.isAuthenticated
+    });
+
+    return connect(mapStateToProps)(AuthenticatedComponent);
+
+}
