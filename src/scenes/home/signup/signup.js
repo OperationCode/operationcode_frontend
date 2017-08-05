b84cb4a2a0bdacd1afca81092534330ccf4d2b19
cooperbuilt <evanncooper@gmail.com>
2017-08-05 19:27:00 -0400

    Cherry Pick

--- src/scenes/home/signup/signup.js
+++ src/scenes/home/signup/signup.js
@@ -183,8 +183,13 @@ class SignUp extends Component {
           {this.state.error ? <ul className={styles.errorList}>There was an error joining Operation Code:
             <li className={styles.errorMessage}>{this.state.error}</li>
           </ul> : null }
+<<<<<<< HEAD
           {this.state.success && <Redirect to="/thanks" />}
           {this.state.isLoading ? <FormButton className={styles.joinButton} text="Loading..." disabled theme="grey" /> : <FormButton className={styles.joinButton} text="Join" onClick={this.handleOnClick} theme="red" />}
+=======
+          {this.state.success && <Redirect to="/signup-info" />}
+          <FormButton className={styles.joinButton} text="Join" onClick={this.handleOnClick} theme="red" />
+>>>>>>> 0558b78... Fix redirect from signup for flow
         </Form>
       </Section>
     );
