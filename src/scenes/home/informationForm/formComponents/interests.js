9af65d83c5e2ac4e8a269b465169a858f238f1ed
cooperbuilt <evanncooper@gmail.com>
2017-08-04 22:05:23 -0400

    Refactor interests component to render checkboxes from array values

--- src/scenes/home/informationForm/formComponents/interests.js
+++ src/scenes/home/informationForm/formComponents/interests.js
@@ -4,43 +4,42 @@ import Form from 'shared/components/form/form';
 import FormCheckBox from 'shared/components/form/formCheckBox/formCheckBox';
 import PropTypes from 'prop-types';
 
+const languageValues = ['Javascript', 'Ruby', 'Java', 'Python', 'C#', 'C', 'Swift', '.NET', 'HTML / CSS'];
+const disciplineValues = ['Web Developer', 'Web Designer', 'Front-End Developer', 'Back-End Developer', 'Full-Stack Developer', 'Mobile / IOS', 'Mobile / Android', 'I.T / SysAdmin', 'Cyber Security', 'Data Science'];
+
 class Interests extends Component {
 
   render() {
+    const languages = languageValues
+      .map(language =>
+        (
+          <FormCheckBox
+            name={'languages'}
+            value={language}
+            onChange={e => this.props.update(e.target.value)}
+            key={language}
+          />
+      )
+    );
+    const disciplines = disciplineValues
+      .map(discipline =>
+        (
+          <FormCheckBox
+            name={'disciplines'}
+            value={discipline}
+            onChange={e => this.props.update(e.target.value)}
+            key={discipline}
+          />
+      )
+    );
     return (
       <Form>
         <span>Progress = {this.props.percent}%</span>
         <Line percent={this.props.percent} strokeWidth="4" strokeColor="green" />
-        <FormCheckBox
-          name={'interests'}
-          value={'Java'}
-          onChange={e => this.props.update(e.target.value)}
-        />
-        <FormCheckBox
-          name={'interests'}
-          value={'Python'}
-          onChange={e => this.props.update(e.target.value)}
-        />
-        <FormCheckBox
-          name={'interests'}
-          value={'Butts'}
-          onChange={e => this.props.update(e.target.value)}
-        />
-        <FormCheckBox
-          name={'interests'}
-          value={'JS'}
-          onChange={e => this.props.update(e.target.value)}
-        />
-        <FormCheckBox
-          name={'interests'}
-          value={'Other Butts'}
-          onChange={e => this.props.update(e.target.value)}
-        />
-        <FormCheckBox
-          name={'interests'}
-          value={'Things'}
-          onChange={e => this.props.update(e.target.value)}
-        />
+        <label htmlFor="languages">Select any languages you are interested in</label>
+        {languages}
+        <label htmlFor="disciplines">Select any disciplines you are interested in</label>
+        {disciplines}
       </Form>
     );
   }
