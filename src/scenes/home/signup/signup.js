e7358fcc39c53d210ffdda191ce50a7c5750f6ff
cooperbuilt <evanncooper@gmail.com>
2017-08-05 19:27:00 -0400

    Cheryy Pick

--- src/scenes/home/signup/signup.js
+++ src/scenes/home/signup/signup.js
@@ -185,9 +185,12 @@ class SignUp extends Component {
           </ul> : null }
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
           {this.state.success && <Redirect to="/thanks" />}
           {this.state.isLoading ? <FormButton className={styles.joinButton} text="Loading..." disabled theme="grey" /> : <FormButton className={styles.joinButton} text="Join" onClick={this.handleOnClick} theme="red" />}
 =======
+=======
+>>>>>>> 0558b78... Fix redirect from signup for flow
 =======
 >>>>>>> 0558b78... Fix redirect from signup for flow
           {this.state.success && <Redirect to="/signup-info" />}
