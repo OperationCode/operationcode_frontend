b0af0930c4f5bbdaa9d7d1a38f7c96ac78ed259a
matthewzurek <matthewzurek1@gmail.com>
2017-08-06 20:27:14 -0500

    Fixes nav dropdown styles

--- src/scenes/home/header/header.js
+++ src/scenes/home/header/header.js
@@ -28,18 +28,22 @@ class Header extends Component {
       <span>
         <NavItem to="/gala" text="Gala 2017" onClick={onClick} />
         <NavItem to="/about" text="About" onClick={onClick} />
-        <NavItem to="/code_schools" text="Code Schools" onClick={onClick} />
+        <NavItem to="/code-schools" text="Code Schools" onClick={onClick} />
         <NavItem to="https://donorbox.org/operationcode" text="Donate" isExternal onClick={onClick} />
-        {!signedIn && <NavDropdown text="Platform">
+        {signedIn && <NavDropdown text="Platform">
           <NavItem to="/mentor-request" text="Request Help" onClick={onClick} />
           <NavItem to="/mentors" text="Mentors" onClick={onClick} />
           {mentor && <NavItem to="/requests" text="Requests" onClick={onClick} />}
           <NavItem to="/squads" text="Squads" onClick={onClick} />
         </NavDropdown>}
-        {signedIn ? <NavItem to="/profile" text="Profile" onClick={onClick} />
-                  : <NavItem to="/join" text="Join" onClick={onClick} />}
-        {signedIn ? <NavItem to="/" text="Logout" onClick={this.props.logOut} />
-                  : <NavItem to="/login" text="Login" onClick={onClick} />}
+        {signedIn ? (<NavDropdown text="User">
+          <NavItem to="/profile" text="Profile" onClick={onClick} />
+          <NavItem to="/" text="Logout" onClick={this.props.logOut} />
+        </NavDropdown>)
+        : (<NavDropdown text="Account">
+          <NavItem to="/join" text="Join" onClick={onClick} />
+          <NavItem to="/login" text="Login" onClick={onClick} />
+        </NavDropdown>)}
       </span>
     );
   }
@@ -50,6 +54,7 @@ class Header extends Component {
       [`${styles.header}`]: true,
       [`${styles.transparent}`]: this.props.transparent
     });
+
     return (
       <div className={classes} >
         <Logo />
--- src/scenes/home/header/navDropdown/navDropdown.js
+++ src/scenes/home/header/navDropdown/navDropdown.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
+import classNames from 'classnames';
 import NavItem from '../navItem/navItem';
 import styles from './navDropdown.css';
 
 class NavDropdown extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       open: false
     };
@@ -17,12 +19,17 @@ class NavDropdown extends Component {
   }
 
   render() {
+    const classes = classNames({
+      [`${styles.child}`]: true,
+      [`${styles.opaque}`]: !(this.props.location.pathname === '/')
+    });
+
     return (
       <span className={styles.parent}>
         <NavItem text={this.props.text} onClick={this.handleToggle} />
         {this.state.open && <div className={styles.children}>
           {this.props.children.map(v => (
-            <span className={styles.child}>
+            <span className={classes}>
               {v}
             </span>
           ))}
@@ -34,7 +41,8 @@ class NavDropdown extends Component {
 
 NavDropdown.propTypes = {
   children: PropTypes.instanceOf(NavItem).isRequired,
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired
 };
 
-export default NavDropdown;
+export default withRouter(NavDropdown);
