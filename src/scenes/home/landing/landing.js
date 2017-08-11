126007b82751cdcadc79df036778faabb402a07f
Ian Roberts <ianroberts131@gmail.com>
2017-08-11 07:54:24 -0700

    replace gala link with banner

--- src/scenes/home/landing/landing.js
+++ src/scenes/home/landing/landing.js
@@ -3,6 +3,7 @@ import LinkButton from 'shared/components/linkButton/linkButton';
 import Donate from 'shared/components/donate/donate';
 import Join from 'shared/components/join/join';
 
+import GalaBanner from './galaBanner/galaBanner';
 import WhatWeDo from './whatWeDo/whatWeDo';
 import Membership from './membership/membership';
 import MoreInformation from './moreInformation/moreInformation';
@@ -18,6 +19,7 @@ class Landing extends Component {
             families launch software development careers.</h1>
           <LinkButton text="Join" theme="red" link="/signup" />
         </div>
+        <GalaBanner />
         <WhatWeDo />
         <Membership />
         <MoreInformation />
