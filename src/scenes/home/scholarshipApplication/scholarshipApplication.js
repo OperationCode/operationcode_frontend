fe07116c271ac72b65e425b4020309ae7d2e75a6
Julien Mayer <jalexismayer@gmail.com>
2017-08-15 13:19:49 -0500

    adds formTextArea

--- src/scenes/home/scholarshipApplication/scholarshipApplication.js
+++ src/scenes/home/scholarshipApplication/scholarshipApplication.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+// import * as CookieHelpers from 'shared/utils/cookieHelper';
+import Form from 'shared/components/form/form';
+// import FormInput from 'shared/components/form/formInput/formInput';
+import FormTextArea from 'shared/components/form/formTextArea/formTextArea';
+import FormCheckbox from 'shared/components/form/formCheckbox/formCheckbox';
+import FormButton from 'shared/components/form/formButton/formButton';
 
 class ScholarshipApplication extends Component {
+  constructor() {
+    super();
+    this.state = {
+      checked: false
+    };
+  }
+
+  onCheckboxChange = (value) => {
+    this.setState({
+      checked: value
+    });
+  }
+
+  isFormValid = () =>
+    this.state.checked;
+
   render() {
+    // console.log(CookieHelpers.getUserId());
     return (
       <div>
-        Application
+        <Form>
+          <FormTextArea />
+          <FormCheckbox onChange={this.onCheckboxChange} />
+          {this.isFormValid() && <FormButton text="Submit Application" />}
+        </Form>
       </div>
     );
   }
---
+++ src/shared/components/form/formTextArea/formTextArea.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class FormTextArea extends Component {
+  render() {
+    return (
+      <div>
+        <textarea>
+          Hell
+        </textarea>
+      </div>
+    );
+  }
+}
