a4f7e4eaa83ed5e275059b7ea42c6331fe41c1e9
Seth Bergman <seth.atxwebs@gmail.com>
2017-08-20 03:00:33 -0500

    host images locally

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,4 +1,3 @@
-
 const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
@@ -20,6 +19,9 @@ const publicPath = '/';
 const publicUrl = '';
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
+
+// mergeStaticsConfig='true'
+// staticFileGlobs=['public/*']
 
 // Options for PostCSS as we reference these options twice
 // Adds vendor prefixing to support IE9 and above
@@ -154,23 +156,24 @@ module.exports = {
           /\.json$/,
           /\.bmp$/,
           /\.gif$/,
-          /\.jpe?g$/,
-          /\.png$/
+          /\.jpg$/,
+          /\.png$/,
+          /\.svg$/
         ],
         loader: require.resolve('file-loader'),
         options: {
-          name: 'static/media/[name].[hash:8].[ext]'
+          name: 'static/media/[name].[ext]'
         }
       },
       // "url" loader works like "file" loader except that it embeds assets
       // smaller than specified limit in bytes as data URLs to avoid requests.
       // A missing `test` is equivalent to a match.
       {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        test: [/\.bmp$/, /\.gif$/, /\.jpg$/, /\.png$/, /\.svg$/],
         loader: require.resolve('url-loader'),
         options: {
           limit: 10000,
-          name: 'static/media/[name].[hash:8].[ext]'
+          name: 'static/media/[name].[ext]'
         }
       },
       // Process JS with Babel.
