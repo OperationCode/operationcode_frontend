ee3977c25dec71f3a2564741f130a669c2cb5238
Seth Bergman <seth.atxwebs@gmail.com>
2017-09-02 00:59:30 -0500

    navbar component

--- src/scenes/home/home.js
+++ src/scenes/home/home.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import * as CookieHelpers from 'shared/utils/cookieHelper';
 import Login from 'shared/components/login/login';
+import NavigationBar from 'shared/components/navBar/navBar';
 import IdmeVerify from 'shared/components/idme/idmeverify/idmeverify';
 import AuthenticatedRoute from 'shared/components/authenticatedRoute/authenticatedRoute';
 import familyImage from 'images/Family-2.jpg';
