f0911b8b7264603b6d91b7dfcb623c84508a5ffb
cooperbuilt <evanncooper@gmail.com>
2017-09-13 11:11:31 -0400

    Add route for jobs page, comment out styles until further notice

--- src/scenes/home/jobs/jobs.js
+++ src/scenes/home/jobs/jobs.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Section from 'shared/components/section/section';
 
-import styles from './jobs.css';
+// import styles from './jobs.css';
 
 class Jobs extends Component {
   render() {
     return (
-      <Section title="Operation Code Gala" theme="white">
+      <Section title="Open Tech Positions" theme="white">
         <div id="zipsearch_container" />
       </Section>
     );
