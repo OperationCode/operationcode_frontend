48e44ba0adb77d4549b2113f2f4c15847d31fd67
cooperbuilt <evanncooper@gmail.com>
2017-09-13 11:08:43 -0400

    Create basic folder structure for jobs page

---
+++ src/scenes/home/jobs/jobs.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Section from 'shared/components/section/section';
+
+import styles from './jobs.css';
+
+class Jobs extends Component {
+  render() {
+    return (
+      <Section title="Operation Code Gala" theme="white">
+        <div id="zipsearch_container" />
+      </Section>
+    );
+  }
+}
+
+export default Jobs;
