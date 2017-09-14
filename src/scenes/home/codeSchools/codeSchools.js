8d8147faa654ecc0172f22da25f0edac92c76cab
hollomancer <conrad@operationcode.org>
2017-09-14 19:55:53 -0400

    Add more references to Discourse in our copy

--- src/scenes/home/codeSchools/codeSchools.js
+++ src/scenes/home/codeSchools/codeSchools.js
@@ -40,7 +40,7 @@ class CodeSchools extends Component {
             <br />
             <br />
             We encourage you to check out the schools below, do your research, and ask fellow
-            software developers in <a href="https://operation-code.slack.com/" target="_blank" rel="noopener noreferrer">Slack</a>.
+            software developers in <a href="https://operation-code.slack.com/" target="_blank" rel="noopener noreferrer">Slack</a> or our <a href="https://community.operationcode.org/" target="_blank" rel="noopener noreferrer">forums</a>.
           </p>
 
           <div className={styles.filterButtonDiv}>
