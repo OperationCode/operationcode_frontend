bda0db499f3b93120e9dc7f59454e204abd45f31
cooperbuilt <evanncooper@gmail.com>
2017-10-01 17:50:16 -0400

    Rename to align with file structure

--- src/scenes/home/profile/profile.js
+++ src/scenes/home/profile/profile.js
@@ -3,31 +3,44 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { FaGraduationCap } from 'react-icons/lib/fa';
 import Section from 'shared/components/section/section';
+import ProfileCard from 'shared/components/profileCard/profileCard';
 import Idme from 'shared/components/idme/idme';
 import discourseImage from '../../../images/Discourse_logo.png';
 import slackImage from '../../../images/slack.png';
 import styles from './profile.css';
 
 class Profile extends Component {
-
   render() {
     return (
       <div>
         <Section title="Scholarship Opportunities" theme="white">
-          <span className={styles.profileText}>Scholarships to attend tech conferences for the military community!</span>
+          <span className={styles.profileText}>
+            Scholarships to attend tech conferences for the military community!
+          </span>
+          <ProfileCard url={'/about'} title={'This is the Profile Card'}>
+            <FaGraduationCap className={styles.profileImage} size={150} />
+          </ProfileCard>
           <Link to="/scholarships">
             <FaGraduationCap className={styles.profileImage} size={150} />
           </Link>
         </Section>
         <Section title="Operation Code Community" theme="white">
-          <span className={styles.profileText}>Visit the Operation Code Community by clicking below.</span>
+          <span className={styles.profileText}>
+            Visit the Operation Code Community by clicking below.
+          </span>
           <a href="https://community.operationcode.org" target="_blank" rel="noopener noreferrer">
-            <img className={styles.profileImage} src={discourseImage} alt="Visit the Operation Code Community" />
+            <img
+              className={styles.profileImage}
+              src={discourseImage}
+              alt="Visit the Operation Code Community"
+            />
           </a>
         </Section>
 
         <Section title="Sign-in with Slack" theme="white">
-          <span className={styles.profileText}>Visit the Operation Code Slack Channel by clicking below.</span>
+          <span className={styles.profileText}>
+            Visit the Operation Code Slack Channel by clicking below.
+          </span>
           <a href="https://operation-code.slack.com" target="_blank" rel="noopener noreferrer">
             <img className={styles.profileImage} alt="Sign in with Slack" src={slackImage} />
           </a>
@@ -36,7 +49,11 @@ class Profile extends Component {
         <Section title="Veteran Status" theme="white">
           <div>
             {this.props.verified && <h2>Your profile is verified with id.me</h2>}
-            {!this.props.verified && <div className={styles.idmeButton}><Idme /></div>}
+            {!this.props.verified && (
+              <div className={styles.idmeButton}>
+                <Idme />
+              </div>
+            )}
           </div>
         </Section>
       </div>
@@ -45,11 +62,11 @@ class Profile extends Component {
 }
 
 Profile.propTypes = {
-  verified: PropTypes.bool
+  verified: PropTypes.bool,
 };
 
 Profile.defaultProps = {
-  verified: false
+  verified: false,
 };
 
 export default Profile;
--- src/shared/components/profileCard/profileCard.js
+++ src/shared/components/profileCard/profileCard.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './profileCard.css';
 
-const profileCard = props => (
+const ProfileCard = props => (
   <Link to={props.url} className={styles.profileCard}>
     {props.children}
     <h2>{props.title}</h2>
   </Link>
 );
 
-profileCard.propTypes = {
+ProfileCard.propTypes = {
   url: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   children: PropTypes.element.isRequired,
 };
 
-export default profileCard;
+export default ProfileCard;
