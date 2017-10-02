17037e87f44b24864cd292f2c94fdebf0dc5d07f
cooperbuilt <evanncooper@gmail.com>
2017-10-02 10:43:30 -0400

    Replace react router link with a plain a tag in linkedIconCard to allow both local and external links

--- src/shared/components/linkedIconCard/linkedIconCard.js
+++ src/shared/components/linkedIconCard/linkedIconCard.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import styles from './linkedIconCard.css';
 
 // Stateless component that wraps a link around an icon / img and title
 // Image or Icon are passed as a child to allow more flexibility
 // Props = url, title, and at least one child
 const linkedIconCard = props => (
-  <Link to={props.url} className={styles.linkedIconCard}>
+  <a href={props.url} className={styles.linkedIconCard}>
     {props.children}
     <span className={styles.linkedIconCard__title}>{props.title}</span>
-  </Link>
+  </a>
 );
 
 linkedIconCard.propTypes = {
