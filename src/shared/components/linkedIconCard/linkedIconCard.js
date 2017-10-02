8cc7927f09ef880fdf14aeb0727e1aaf11a06269
cooperbuilt <evanncooper@gmail.com>
2017-10-02 10:28:44 -0400

    Rename ProfileCard to LinkedIconCard to encourage re-useability

--- src/shared/components/linkedIconCard/linkedIconCard.js
+++ src/shared/components/linkedIconCard/linkedIconCard.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import styles from './profileCard.css';
+import styles from './linkedIconCard.css';
 
 // Stateless component that wraps a link around an icon / img and title
 // Image or Icon are passed as a child to allow more flexibility
 // Props = url, title, and at least one child
-const ProfileCard = props => (
-  <Link to={props.url} className={styles.profileCard}>
+const linkedIconCard = props => (
+  <Link to={props.url} className={styles.linkedIconCard}>
     {props.children}
-    <span className={styles.profileCard__title}>{props.title}</span>
+    <span className={styles.linkedIconCard__title}>{props.title}</span>
   </Link>
 );
 
-ProfileCard.propTypes = {
+linkedIconCard.propTypes = {
   url: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   children: PropTypes.element.isRequired,
 };
 
-export default ProfileCard;
+export default linkedIconCard;
