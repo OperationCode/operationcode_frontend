98ed2267f317cf7457b33e56cab015e65dff0e14
HasGisete <gisete@tune.com>
2017-10-02 10:40:20 -0700

    Force TimelineEvent.js Title to be TitleCase with Regex #531

--- src/scenes/home/history/timelineEvent/timelineEvent.js
+++ src/scenes/home/history/timelineEvent/timelineEvent.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 import styles from './timelineEvent.css';
 
 class TimelineEvent extends Component {
+  titleCase = title => title.toLowerCase().replace(/(^[a-z]| [a-z])/g, match => match.toUpperCase());
+
   render() {
     return (
       <div className={styles.eventContainer}>
         <h4 className={styles.eventTitle}>
-          {this.props.title}
+          {this.titleCase(this.props.title)}
         </h4>
-
         <div className={styles.eventContent}>
           <p>
             {this.props.content}
