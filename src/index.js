860caa18955c0e975360f78f5f7f3b645db965b2
Michael Kirschbaum <me@michaelkirschbaum.com>
2017-10-01 21:44:04 -0700

    create provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import 'font-awesome/css/font-awesome.css';
 import App from './App';
 import './shared/styles/normalize.global.css';
 import './shared/styles/react-table.global.css';
 import './shared/styles/react-select.global.css';
 import './index.css';
+import store from './state/store';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/state/store.js
@@ -0,0 +1,7 @@
+import { createStore, combineReducers } from 'redux';
+
+const reducers = combineReducers({});
+
+const store = createStore(reducers);
+
+export default store;
