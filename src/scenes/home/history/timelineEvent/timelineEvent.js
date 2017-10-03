258fbf47de17239616c27c52352fa2ced470c6bf
HasGisete <gisete@tune.com>
2017-10-02 22:55:27 -0700

    Revert "Force TimelineEvent.js Title to be TitleCase with Regex #531"
    This reverts commit 98ed2267f317cf7457b33e56cab015e65dff0e14.

--- src/scenes/home/history/timelineEvent/timelineEvent.js
+++ src/scenes/home/history/timelineEvent/timelineEvent.js
@@ -3,14 +3,13 @@ import PropTypes from 'prop-types';
 import styles from './timelineEvent.css';
 
 class TimelineEvent extends Component {
-  titleCase = title => title.toLowerCase().replace(/(^[a-z]| [a-z])/g, match => match.toUpperCase());
-
   render() {
     return (
       <div className={styles.eventContainer}>
         <h4 className={styles.eventTitle}>
-          {this.titleCase(this.props.title)}
+          {this.props.title}
         </h4>
+
         <div className={styles.eventContent}>
           <p>
             {this.props.content}
