5425865e1d28a3703b0b32f4be708a2c41cec390
Michael Kirschbaum <me@michaelkirschbaum.com>
2017-10-03 16:41:22 -0700

    separate actions from creators

--- src/state/registration/actions.js
+++ src/state/registration/actions.js
@@ -1,11 +1,15 @@
+const ADD_EMAIL = 'ADD_EMAIL'
+const ADD_PASSWORD = 'ADD_PASSWORD'
+const ADD_ZIP = 'ADD_ZIP'
+
 export function addEmail(email) {
-  return { type: 'ADD_EMAIL', email }
+  return { type: ADD_EMAIL, email }
 }
 
 export function addPassword(password) {
-  return { type: 'ADD_PASSWORD', password }
+  return { type: ADD_PASSWORD, password }
 }
 
 export function addZip(zip) {
-  return { type: 'ADD_ZIP', zip }
+  return { type: ADD_ZIP, zip }
 }
--- src/state/registration/reducers.js
+++ src/state/registration/reducers.js
@@ -1,3 +1,16 @@
+import { connect } from 'react-redux';
+import { ADD_EMAIL, ADD_PASSWORD, ADD_ZIP } from './actions'
+
+const intialState = null
+
 function register(state = initialState, action) {
-  return state
+  switch(action.type)
+    case ADD_EMAIL:
+      return state
+    case ADD_PASSWORD:
+      return state
+    case ADD_ZIP:
+      return state
+    default:
+      return state
 }
