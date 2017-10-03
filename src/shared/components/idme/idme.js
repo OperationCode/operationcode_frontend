733c9c55765a7ec05c36f3422b39cce98b99f951
cooperbuilt <evanncooper@gmail.com>
2017-10-03 13:02:59 -0400

    Remove componentWillMount from idMe.js shared component

--- src/shared/components/idme/idme.js
+++ src/shared/components/idme/idme.js
@@ -4,7 +4,6 @@ import troopImage from 'images/Troop.png';
 import styles from './idme.css';
 
 class Idme extends Component {
-  componentWillMount() {}
   idMe = () => {
     window.open(
       `${config.idmeOAuthUrl}?client_id=${config.idmeClientId}&redirect_uri=${config.host}/profile/verify&response_type=token&scope=military&display=popup', '', 'scrollbars=yes,menubar=no,status=no,location=no,toolbar=no,width=750,height=780,top=200,left=200`
