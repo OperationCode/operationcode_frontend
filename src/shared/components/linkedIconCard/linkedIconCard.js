46873487934f71254f35524108426468fabb4e75
cooperbuilt <evanncooper@gmail.com>
2017-10-03 08:02:11 -0400

    Titlecase LinkedIconCard in file structure

--- src/shared/components/linkedIconCard/linkedIconCard.js
+++ src/shared/components/linkedIconCard/linkedIconCard.js
@@ -5,17 +5,17 @@ import styles from './linkedIconCard.css';
 // Stateless component that wraps a link around an icon / img and title
 // Image or Icon are passed as a child to allow more flexibility
 // Props = url, title, and at least one child
-const linkedIconCard = props => (
+const LinkedIconCard = props => (
   <a href={props.url} className={styles.linkedIconCard}>
     {props.children}
     <span className={styles.linkedIconCard__title}>{props.title}</span>
   </a>
 );
 
-linkedIconCard.propTypes = {
+LinkedIconCard.propTypes = {
   url: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   children: PropTypes.element.isRequired,
 };
 
-export default linkedIconCard;
+export default LinkedIconCard;
