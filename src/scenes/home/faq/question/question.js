0d7fefab6c432c2dcf9403a32fcc3fb7dac5d7bb
Ashutosh Tripathi <ashu17706@gmail.com>
2017-10-04 12:29:05 +0530

    corrected eslint errors

--- src/scenes/home/faq/question/question.js
+++ src/scenes/home/faq/question/question.js
@@ -2,17 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './question.css';
 
-const Question = () => {
-  return (
-    <div className={styles.accordionSingle}>
-      <input className={styles.accordionSingleHidden} type="checkbox" id={this.props.q} />
-      <label className={styles.accordionSingleQuestion} htmlFor={this.props.q}> {this.props.q}</label>
-      { // eslint-disable-next-line
-        <p className={styles.accordionSingleAnswer} dangerouslySetInnerHTML={{ __html: this.props.a }} />
-      }
-    </div>
-  );
-}
+const Question = ({ q, a }) => (
+  <div className={styles.accordionSingle}>
+    <input className={styles.accordionSingleHidden} type="checkbox" id={q} />
+    <label className={styles.accordionSingleQuestion} htmlFor={q}> {q}</label>
+    { // eslint-disable-next-line
+      <p className={styles.accordionSingleAnswer} dangerouslySetInnerHTML={{ __html: a }} />
+    }
+  </div>
+);
 
 Question.propTypes = {
   q: PropTypes.string.isRequired,
