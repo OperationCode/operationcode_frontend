d874dacd0b01146245e3fd7db38028f07ac326d7
Ashutosh Tripathi <ashu17706@gmail.com>
2017-10-04 10:30:50 +0530

    convert question to SFC

--- src/scenes/home/faq/question/question.js
+++ src/scenes/home/faq/question/question.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './question.css';
 
-class Question extends Component {
-  render() {
-    return (
-      <div className={styles.accordionSingle}>
-        <input className={styles.accordionSingleHidden} type="checkbox" id={this.props.q} />
-        <label className={styles.accordionSingleQuestion} htmlFor={this.props.q}> {this.props.q}</label>
-        { // eslint-disable-next-line
-          <p className={styles.accordionSingleAnswer} dangerouslySetInnerHTML={{ __html: this.props.a }} />
-        }
-      </div>
-    );
-  }
+const Question = () => {
+  return (
+    <div className={styles.accordionSingle}>
+      <input className={styles.accordionSingleHidden} type="checkbox" id={this.props.q} />
+      <label className={styles.accordionSingleQuestion} htmlFor={this.props.q}> {this.props.q}</label>
+      { // eslint-disable-next-line
+        <p className={styles.accordionSingleAnswer} dangerouslySetInnerHTML={{ __html: this.props.a }} />
+      }
+    </div>
+  );
 }
 
 Question.propTypes = {
