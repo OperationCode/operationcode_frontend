f7f78940b5dbe5f8345fd4eceb89b2663dcb6db7
K MacLeod <ksmacleod99@gmail.com>
2017-10-04 13:50:13 -0400

    Updates #578
    Removes Gala banner, updates Gala information page

--- src/scenes/home/landing/landing.js
+++ src/scenes/home/landing/landing.js
@@ -3,7 +3,6 @@ import LinkButton from 'shared/components/linkButton/linkButton';
 import Donate from 'shared/components/donate/donate';
 import Join from 'shared/components/join/join';
 
-import GalaBanner from './galaBanner/galaBanner';
 import WhatWeDo from './whatWeDo/whatWeDo';
 import Membership from './membership/membership';
 import MoreInformation from './moreInformation/moreInformation';
@@ -19,7 +18,6 @@ class Landing extends Component {
             families launch software development careers.</h1>
           <LinkButton text="Join" theme="red" link="/signup" />
         </div>
-        <GalaBanner />
         <WhatWeDo />
         <Membership />
         <MoreInformation />
