ae493b203ee4310918ba90f8a11fa6a059c2c885
Michael Kirschbaum <me@michaelkirschbaum.com>
2017-10-04 14:51:42 -0700

    create reducer 'login'

---
+++ src/state/login/reducers.js
@@ -0,0 +1,21 @@
+import { ADD_EMAIL, ADD_PASSWORD } from './actions';
+
+const intialState = {
+  email: '',
+  password: ''
+}
+
+export default function login(state = initialState, action) {
+  switch (action.type) {
+    case ADD_EMAIL:
+      return state;
+    case ADD_PASSWORD:
+      return Object.assign({}, state, {
+        email: action.email
+      });
+    default:
+      return Object.assign({}, state, {
+        password: action.password
+      });
+  }
+}
