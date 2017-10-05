a7692293c2657ee2408577aa246b09df57536a50
Drew Bragg <DRBragg@gmail.com>
2017-10-04 20:12:27 -0400

    converted stateless header components to functions

--- src/scenes/home/header/sideNav/sideNav.js
+++ src/scenes/home/header/sideNav/sideNav.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import logo from 'images/logos/small-white-logo.png';
 import Drawer from 'shared/components/drawer/drawer';
 import styles from './sideNav.css';
 
-class SideNav extends Component {
-  render() {
-    const { isVisible } = this.props;
-    return (
-      <Drawer isVisible={isVisible}>
-        <div className={styles.content}>
-          <div className={styles.header}>
-            <a href="/">
-              <img className={styles.logo} src={logo} alt="Operation Code logo" />
-            </a>
-            <a className={styles.close} href="/" onClick={this.props.onClose}>&#10006;</a>
-          </div>
-          <div className={styles.list}>
-            {this.props.children}
-          </div>
+const SideNav = (props) => {
+  const { isVisible } = props;
+
+  return (
+    <Drawer isVisible={isVisible}>
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <a href="/">
+            <img className={styles.logo} src={logo} alt="Operation Code logo" />
+          </a>
+          <a className={styles.close} href="/" onClick={props.onClose}>&#10006;</a>
         </div>
-      </Drawer>
-    );
-  }
-}
+        <div className={styles.list}>
+          {props.children}
+        </div>
+      </div>
+    </Drawer>
+  );
+};
 
 SideNav.propTypes = {
   children: PropTypes.node.isRequired,
