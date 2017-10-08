c9ba84f3612096deb0f1d3a66e7e53cac84a65ce
Nestor Segura <nestor.segura@gmail.com>
2017-10-08 14:33:18 +0200

    implement sugested changes to login/login.js/css, and add signUpSection.js

--- src/shared/components/login/login.js
+++ src/shared/components/login/login.js
@@ -1,9 +1,9 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import config from 'config/environment';
-import _ from 'lodash';
+import { Link } from 'react-router-dom';
 import styles from './login.css';
 import Form from '../form/form';
 import Section from '../section/section';
@@ -11,7 +11,7 @@ import * as CookieHelpers from '../../utils/cookieHelper';
 import FormEmail from '../form/formEmail/formEmail';
 import FormInput from '../form/formInput/formInput';
 import FormButton from '../form/formButton/formButton';
-import SignUpLink from '../signUpLink/signUpLink';
+import SignUpSection from './signUpSection';
 
 require('./login.css');
 const queryString = require('query-string');
@@ -29,25 +29,25 @@ class Login extends Component {
     sso: null,
     sig: null,
     ssoParamsPresent: false
-  }
+  };
 
   componentDidMount = () => {
     this.checkForSsoParams();
-  }
+  };
 
   onEmailChange = (value, valid) => {
     this.setState({ email: value, emailValid: valid });
-  }
+  };
 
   onPasswordChange = (value, valid) => {
     this.setState({ password: value, passwordValid: valid });
-  }
+  };
 
   setErrorMessage = (error) => {
     const errorStatus = _.get(error, ['response', 'status'], -1);
     const errorMessage = _.get(error, 'message');
     this.setState({ errorStatus, errorMessage });
-  }
+  };
 
   setSsoParams = () => {
     const parsed = queryString.parse(window.location.search);
@@ -61,7 +61,7 @@ class Login extends Component {
         this.checkSsoLoggedIn
       );
     }
-  }
+  };
 
   // SSO Flow:
   //   * Discourse sends us an SSO token and a signature for that token (shared key)
@@ -84,65 +84,74 @@ class Login extends Component {
     const parsed = queryString.parse(window.location.search);
 
     if (parsed.sso && parsed.sig) {
-      this.setState(
-        { ssoParamsPresent: true },
-        this.setSsoParams
-      );
+      this.setState({ ssoParamsPresent: true }, this.setSsoParams);
     }
-  }
+  };
 
   checkSsoLoggedIn = () => {
-    if (this.state.ssoParamsPresent && this.props.isLoggedIn) { this.ssoLoggedInRedirect(); }
-  }
+    if (this.state.ssoParamsPresent && this.props.isLoggedIn) {
+      this.ssoLoggedInRedirect();
+    }
+  };
 
   ssoLoggedInRedirect = () => {
-    axios.get(`${config.backendUrl}/sessions/sso?sso=${encodeURI(this.state.sso)}&sig=${this.state.sig}`, {
-      headers: {
-        Authorization: `Bearer ${CookieHelpers.authToken()}`
-      }
-    }).then(({ data }) => {
-      window.location = data.redirect_to;
-    }).catch((error) => {
-      this.setErrorMessage(error);
-    });
-  }
+    axios
+      .get(
+        `${config.backendUrl}/sessions/sso?sso=${encodeURI(this.state.sso)}&sig=${this.state.sig}`,
+        {
+          headers: {
+            Authorization: `Bearer ${CookieHelpers.authToken()}`
+          }
+        }
+      )
+      .then(({ data }) => {
+        window.location = data.redirect_to;
+      })
+      .catch((error) => {
+        this.setErrorMessage(error);
+      });
+  };
 
-  isFormValid = () => this.state.emailValid && this.state.passwordValid
+  isFormValid = () => this.state.emailValid && this.state.passwordValid;
 
   // This is a temp function to ensure we return a URL if the changes to the API
   // aren't in place. It can be removed after operationcode_backend#91 has been deployed
   resolveRedirectUrl = (redirectUrl) => {
-    if (redirectUrl) { return redirectUrl; }
+    if (redirectUrl) {
+      return redirectUrl;
+    }
     return '/profile';
-  }
-
+  };
 
   handleOnClick = (e) => {
     e.preventDefault();
 
     if (this.isFormValid()) {
-      axios.post(`${config.backendUrl}/sessions`, {
-        user: {
-          email: this.state.email,
-          password: this.state.password,
-        },
-        sso: this.state.sso,
-        sig: this.state.sig
-      }).then(({ data }) => {
-        CookieHelpers.setUserAuthCookie(data);
-        this.setState({ authenticated: true });
-        this.props.updateRootAuthState((history) => {
-          if (this.state.ssoParamsPresent) {
-            window.location = data.redirect_to;
-          } else {
-            history.push(this.resolveRedirectUrl(data.redirect_to));
-          }
+      axios
+        .post(`${config.backendUrl}/sessions`, {
+          user: {
+            email: this.state.email,
+            password: this.state.password
+          },
+          sso: this.state.sso,
+          sig: this.state.sig
+        })
+        .then(({ data }) => {
+          CookieHelpers.setUserAuthCookie(data);
+          this.setState({ authenticated: true });
+          this.props.updateRootAuthState((history) => {
+            if (this.state.ssoParamsPresent) {
+              window.location = data.redirect_to;
+            } else {
+              history.push(this.resolveRedirectUrl(data.redirect_to));
+            }
+          });
+        })
+        .catch((error) => {
+          this.setErrorMessage(error);
         });
-      }).catch((error) => {
-        this.setErrorMessage(error);
-      });
     }
-  }
+  };
 
   render() {
     const { errorStatus, errorMessage } = this.state;
@@ -154,20 +163,30 @@ class Login extends Component {
     }
 
     return (
-      <Section title="Login" theme="white">
-        <Form autoComplete>
-          <FormEmail id="email" displayName="Email" label="Email" onChange={this.onEmailChange} />
-          <FormInput id="password" displayName="Password" label="Password" inputType="password" onChange={this.onPasswordChange} />
-          {errorFeedback && <h2 className={styles.loginError}>{errorFeedback}</h2>}
-          <FormButton className={styles.Button} text="Login" onClick={this.handleOnClick} />
-        </Form>
-        <Link to="/reset_password">Reset Password</Link>
-        <SignUpLink />
-      </Section>
+      <div className={styles.gridRow}>
+        <Section title="Login" theme="white">
+          <Form autoComplete>
+            <FormEmail id="email" displayName="Email" label="Email" onChange={this.onEmailChange} />
+            <FormInput
+              id="password"
+              displayName="Password"
+              label="Password"
+              inputType="password"
+              onChange={this.onPasswordChange}
+            />
+            {errorFeedback && <h2 className={styles.loginError}>{errorFeedback}</h2>}
+            <FormButton className={styles.Button} text="Login" onClick={this.handleOnClick} />
+            <Link className={styles.resetBtn} to="/reset_password">
+              Reset Password
+            </Link>
+          </Form>
+        </Section>
+
+        <SignUpSection />
+      </div>
     );
   }
 }
-
 
 Login.propTypes = {
   updateRootAuthState: PropTypes.func,
