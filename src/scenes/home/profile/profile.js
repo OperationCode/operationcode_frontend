6d4406e51b295336e7d27ea4f6ec9c4c9bd7efb1
Kyle Holmberg <inbox@kylemh.com>
2017-10-08 23:42:05 -0700

    Add linted changes

--- src/scenes/home/profile/profile.js
+++ src/scenes/home/profile/profile.js
@@ -5,38 +5,43 @@ import IconCard from 'shared/components/iconCard/iconCard';
 import Idme from 'shared/components/idme/idme';
 import styles from './profile.css';
 
-
 const Profile = ({ verified }) => (
   <Section title="My Profile" theme="gray">
     <div className={styles.profileContainer}>
       <div className={styles.profileContainer__cards}>
         <IconCard
-          title="Request a Mentor" fontAwesomeIcon="FaLifeBouy"
+          title="Request a Mentor" 
+          fontAwesomeIcon="FaLifeBouy"
           url="https://op.co.de/mentor-request"
         />
 
         <IconCard
-          title="Check for Scholarships" fontAwesomeIcon="FaGraduationCap"
+          title="Check for Scholarships" 
+          fontAwesomeIcon="FaGraduationCap"
           url="/scholarships"
         />
 
         <IconCard
-          title="Check for Open Jobs" fontAwesomeIcon="FaBriefcase"
+          title="Check for Open Jobs" 
+          fontAwesomeIcon="FaBriefcase"
           url="/jobs"
         />
 
         <IconCard
-          title="Enter our Slack Channel" fontAwesomeIcon="FaSlack"
+          title="Enter our Slack Channel" 
+          fontAwesomeIcon="FaSlack"
           url="https://operation-code.slack.com/"
         />
 
         <IconCard
-          title="Enter our Discourse Forums" fontAwesomeIcon="FaCommentsO"
+          title="Enter our Discourse Forums" 
+          fontAwesomeIcon="FaCommentsO"
           url="https://community.operationcode.org/"
         />
 
         <IconCard
-          title="Update Info" fontAwesomeIcon="FaUser"
+          title="Update Info" 
+          fontAwesomeIcon="FaUser"
           url="/signup-info"
         />
       </div>
