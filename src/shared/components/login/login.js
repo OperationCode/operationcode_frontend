e1317356046b85709abeafa13d91166682dcaff1
Kyle Holmberg <inbox@kylemh.com>
2017-10-09 00:08:30 -0700

    Add requested changes

--- src/shared/components/login/login.js
+++ src/shared/components/login/login.js
@@ -1,5 +1,5 @@
-import _ from 'lodash';
 import React, { Component } from 'react';
+import _ from 'lodash';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import config from 'config/environment';
@@ -50,7 +50,7 @@ class Login extends Component {
   };
 
   setSsoParams = () => {
-    const parsed = queryString.parse(window.location.search);
+    const parsed = queryString.parse(location.search);
 
     if (this.state.ssoParamsPresent) {
       this.setState(
@@ -81,7 +81,7 @@ class Login extends Component {
   //       to discourse with our payload and sig
   //     * These values are provided by the backend
   checkForSsoParams = () => {
-    const parsed = queryString.parse(window.location.search);
+    const parsed = queryString.parse(location.search);
 
     if (parsed.sso && parsed.sig) {
       this.setState({ ssoParamsPresent: true }, this.setSsoParams);
