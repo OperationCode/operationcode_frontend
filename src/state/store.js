b9f212d078c4bd38e1a26bd8fc59f3034997b61c
Michael Kirschbaum <me@michaelkirschbaum.com>
2017-10-10 08:44:10 -0700

    add devtools extension

--- src/state/store.js
+++ src/state/store.js
@@ -1,6 +1,11 @@
 import { createStore } from 'redux';
 import login from './login/reducers';
 
-const store = createStore(login);
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  login,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable */
 
 export default store;
