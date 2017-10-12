167a2991de1fa141849ccc19548f99be393b926b
Kyle Holmberg <inbox@kylemh.com>
2017-10-11 18:31:19 -0700

    Remove unnecessary newlines

--- stories/components/iconCard/index.js
+++ stories/components/iconCard/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-
 import IconCard from 'shared/components/iconCard/iconCard';
-
 
 storiesOf('shared/components/iconCard', module)
   .add('Default', () => (
