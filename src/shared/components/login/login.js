641e9b20b15480d3c71e13af699c9274432fe7f0
Michael Kirschbaum <me@michaelkirschbaum.com>
2017-10-11 19:39:49 -0700

    call ADD_EMAIL action

--- src/shared/components/login/login.js
+++ src/shared/components/login/login.js
@@ -36,7 +36,8 @@ class Login extends Component {
   };
 
   onEmailChange = (value, valid) => {
-    this.setState({ email: value, emailValid: valid });
+    this.props.addEmail(value);
+    this.setState({ emailValid: valid });
   };
 
   onPasswordChange = (value, valid) => {
@@ -191,7 +192,7 @@ class Login extends Component {
 Login.propTypes = {
   updateRootAuthState: PropTypes.func,
   isLoggedIn: PropTypes.bool,
-  // addEmail: PropTypes.func.isRequired
+  addEmail: PropTypes.func.isRequired
 };
 
 Login.defaultProps = {
