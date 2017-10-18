59e93a0acdd0731a816bb5aededdc8f162445cf0
Ron Hansen <rhansen@elbgoods.de>
2017-10-18 23:08:05 +0200

    Add a flash message notification system

--- src/scenes/home/home.js
+++ src/scenes/home/home.js
@@ -33,6 +33,11 @@ import SignupInformation from './informationForm/informationForm';
 import Benefit from './benefit/benefit';
 import styles from './home.css';
 
+const ReactToastr = require('react-toastr');
+
+const { ToastContainer } = ReactToastr;
+const ToastMessageFactory = React.createFactory(ReactToastr.ToastMessage.animation);
+
 class Home extends Component {
   state = {
     bgImage: false,
@@ -51,9 +56,14 @@ class Home extends Component {
 
   setBgImage(location) {
     if (location.pathname === '/') {
-      this.setState({ bgChanged: !(this.state.bgImage), bgImage: true, bgImageUrl: familyImage });
+      this.setState({
+        bgChanged: !(this.state.bgImage),
+        bgImage: true,
+        bgImageUrl: familyImage,
+      });
     } else {
-      this.setState({ bgChanged: this.state.bgImage, bgImage: false, bgImageUrl: null });
+      this.setState(
+        { bgChanged: this.state.bgImage, bgImage: false, bgImageUrl: null });
     }
   }
 
@@ -62,7 +72,7 @@ class Home extends Component {
     this.setState({
       signedIn: cookies.signedIn,
       mentor: cookies.mentor,
-      verified: cookies.verified
+      verified: cookies.verified,
     }, () => {
       if (cb) {
         cb(this.props.history);
@@ -75,8 +85,21 @@ class Home extends Component {
     this.setState({
       signedIn: false,
       mentor: false,
-      verified: false
-    }, () => this.props.history.push('/'));
+      verified: false,
+    }, () => {
+      this.props.history.push('/');
+    });
+  }
+
+  sendNotification = (type, title, subtitle) => {
+    this.container[type](
+      subtitle,
+      title,
+      {
+        timeOut: 3000,
+        extendedTimeOut: 3000,
+      },
+    );
   }
 
   render() {
@@ -84,20 +107,25 @@ class Home extends Component {
     const authProps = {
       signedIn,
       mentor,
-      verified
+      verified,
     };
 
     const classes = classNames({
       [`${styles.home}`]: true,
-      [`${styles.backgroundImage}`]: this.state.bgImage
+      [`${styles.backgroundImage}`]: this.state.bgImage,
     });
     return (
       <div
         className={classes}
-        style={(this.state.bgImage) ? { backgroundImage: `url(${this.state.bgImageUrl})` } : {}}
+        style={(this.state.bgImage)
+          ? { backgroundImage: `url(${this.state.bgImageUrl})` }
+          : {}}
       >
-        <Header transparent={this.state.bgImage} logOut={this.logOut} signedIn={signedIn} mentor={mentor} />
-        <div className={styles.main} >
+        <Header
+          transparent={this.state.bgImage} logOut={this.logOut}
+          signedIn={signedIn} mentor={mentor}
+        />
+        <div className={styles.main}>
           <Switch>
             <Route
               path="/code-schools"
@@ -110,13 +138,21 @@ class Home extends Component {
             <Route
               path="/signup"
               render={() => (
-                <SignUp updateRootAuthState={this.updateRootAuthState} isLoggedIn={this.state.signedIn} {...authProps} />
+                <SignUp
+                  updateRootAuthState={this.updateRootAuthState}
+                  isLoggedIn={this.state.signedIn} {...authProps}
+                  sendNotification={this.sendNotification}
+                />
               )}
             />
             <Route
               path="/join"
               render={() => (
-                <SignUp updateRootAuthState={this.updateRootAuthState} isLoggedIn={this.state.signedIn} {...authProps} />
+                <SignUp
+                  updateRootAuthState={this.updateRootAuthState}
+                  isLoggedIn={this.state.signedIn} {...authProps}
+                  sendNotification={this.sendNotification}
+                />
               )}
             />
             <Route
@@ -207,7 +243,11 @@ class Home extends Component {
             <Route
               path="/login"
               render={() => (
-                <Login updateRootAuthState={this.updateRootAuthState} isLoggedIn={this.state.signedIn} {...authProps} />
+                <Login
+                  updateRootAuthState={this.updateRootAuthState}
+                  isLoggedIn={this.state.signedIn}{...authProps}
+                  sendNotification={this.sendNotification}
+                />
               )}
             />
             <AuthenticatedRoute
@@ -233,6 +273,11 @@ class Home extends Component {
             />
           </Switch>
         </div>
+        <ToastContainer
+          ref={(input) => { this.container = input; }}
+          toastMessageFactory={ToastMessageFactory}
+          className="toast-top-right"
+        />
         <Footer />
       </div>
     );
@@ -241,7 +286,7 @@ class Home extends Component {
 
 Home.propTypes = {
   history: PropTypes.object.isRequired,
-  location: PropTypes.object.isRequired
+  location: PropTypes.object.isRequired,
 };
 
 export default withRouter(Home);
