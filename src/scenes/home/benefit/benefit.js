5cc0366de31b42655d1b5ebdb0ae790ab7091d87
K MacLeod <ksmacleod99@gmail.com>
2017-11-07 16:36:21 -0500

    Update benefit.js

--- src/scenes/home/benefit/benefit.js
+++ src/scenes/home/benefit/benefit.js
@@ -234,7 +234,7 @@ const Benefit = () => (
             href="https://www.topcoder.com/"
             target="_blank"
             rel="noopener noreferrer"
-           >
+          >
             <img className={styles.logoLong} src={topCoderLogo} alt="Top Coder" />
           </a>
           <a
