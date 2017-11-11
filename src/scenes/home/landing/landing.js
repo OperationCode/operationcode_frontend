4d2a9188cbdc3b4641b00de3fc67a153b0334c3c
Kyle Holmberg <kylemh.email@gmail.com>
2017-11-10 16:22:21 -0800

    Implement modal on landing to announce live stream

--- src/scenes/home/landing/landing.js
+++ src/scenes/home/landing/landing.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactModal from 'react-modal';
+import { FaClose } from 'react-icons/lib/fa';
 import LinkButton from 'shared/components/linkButton/linkButton';
 import AnnounceBanner from 'shared/components/announceBanner/announceBanner';
 import WhatWeDo from './whatWeDo/whatWeDo';
@@ -12,29 +14,80 @@ import BenefitBanner from '../../../images/benefit.jpg';
 import MobileBenefitBanner from '../../../images/benefit-mobile.jpg';
 import styles from './landing.css';
 
-const Landing = () => (
-  <div className={styles.landing}>
-    <div className={styles.pageHeading}>
-      <h1>
-        The largest community dedicated to helping military veterans and families launch software
-        development careers.
-      </h1>
-      <LinkButton text="Join" theme="red" link="/signup" />
-    </div>
-    <AnnounceBanner
-      link="/benefit"
-      imageSource={BenefitBanner}
-      fallbackImage450pxWideSource={MobileBenefitBanner}
-      altText="Click here to find more information about our Benefit Dinner and Silent Auction on November 10th"
-    />
-    <WhatWeDo />
-    <Membership />
-    <MoreInformation />
-    <SuccessStories />
-    <Partners />
-    <Donate />
-    <Join />
-  </div>
-);
+class Landing extends Component {
+  state = {
+    showModal: false
+  };
+
+  componentDidMount() {
+    this.toggleModal();
+  }
+
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+
+  render() {
+    return (
+      <div className={styles.landing}>
+        {/* TODO: REMOVE MODAL AFTER BENEFIT DINNER */}
+        <ReactModal
+          isOpen={this.state.showModal}
+          onRequestClose={this.toggleModal}
+          contentLabel="Live Stream of Benefit Dinner"
+          className={styles.liveStreamModal}
+        >
+          <FaClose
+            size={48}
+            className={styles.liveStreamModal_CloseButton}
+            onClick={this.toggleModal}
+          />
+          <div className={styles.liveStreamModal_Content}>
+            <h3>Operation Code Benefit Dinner!</h3>
+            <p>From the bottom of our hearts, thank you for all of your support.</p>
+            <p>
+              We&apos;ll be live streaming the event for your viewing enjoyment. If you&apos;re
+              still interested in donating to Operation Code, please{' '}
+              <a
+                href="https://opencollective.com/operationcode#support"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                click here
+              </a>!
+            </p>
+            <LinkButton
+              text="Link to Live Stream"
+              link="https://www.youtube.com/watch?v=aYcqMZBDUsU"
+              isExternal
+            />
+          </div>
+        </ReactModal>
+        {/* ************************************************************************** */}
+
+        <div className={styles.pageHeading}>
+          <h1>
+            The largest community dedicated to helping military veterans and families launch
+            software development careers.
+          </h1>
+          <LinkButton text="Join" theme="red" link="/signup" />
+        </div>
+        <AnnounceBanner
+          link="/benefit"
+          imageSource={BenefitBanner}
+          fallbackImage450pxWideSource={MobileBenefitBanner}
+          altText="Click here to find more information about our Benefit Dinner and Silent Auction on November 10th"
+        />
+        <WhatWeDo />
+        <Membership />
+        <MoreInformation />
+        <SuccessStories />
+        <Partners />
+        <Donate />
+        <Join />
+      </div>
+    );
+  }
+}
 
 export default Landing;
