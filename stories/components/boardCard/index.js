6a01e5f8ffc66dd7dcebdae3220eda469ca05555
Kyle Holmberg <kylemh.email@gmail.com>
2017-12-06 21:15:59 -0800

    Update storybook reference

--- stories/components/boardCard/index.js
+++ stories/components/boardCard/index.js
@@ -5,12 +5,11 @@ import BoardCard from 'shared/components/boardCard/boardCard';
 
 import jamesBondJpg from '../../asset/james-bond.jpg';
 
-storiesOf('shared/components/boardCard', module)
-  .add('Default', () => (
-    <BoardCard
-      name="James Bond"
-      src={jamesBondJpg}
-      alt="james-bond"
-      role="developer"
-    />
-  ));
+storiesOf('shared/components/boardCard', module).add('Default', () => (
+  <BoardCard
+    name="James Bond"
+    src={jamesBondJpg}
+    description="Sometimes played by Pierce Brosnan, Sean Connery, and multiple other talented actors. Always gets the girl."
+    role="developer"
+  />
+));
