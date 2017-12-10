cc94f8c4fcbce848e07c6c131eea83861c941992
PixelWitch <21322920+PixelWitch@users.noreply.github.com>
2017-12-09 22:57:36 -0500

    problem converting the buttons back into stateless functional components due to me needing props, feedback on google.js would be appreciated

--- src/shared/components/login/google.js
+++ src/shared/components/login/google.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import config from 'config/environment';
 import GoogleLogin from 'react-google-login';
-import SocialLogin from './socialLogin';
+// import SocialLogin from './socialLogin';
 import styles from './socialMediaButtons.css';
 
-class Google extends Component {
-  responseGoogle = (response) => {
-    const login = new SocialLogin(this.props);
-    login.run(response.profileObj.givenName, response.profileObj.familyName, response.profileObj.email);
-  };
-
-  render() {
-    return (
-      <div>
-        <script src="https://apis.google.com/js/platform.js" async defer />
-        <GoogleLogin
-          clientId={`${config.googleKey}`}
-          className={[styles.loginButton, styles.googleButton].join(' ')}
-          onSuccess={this.responseGoogle}
-        />
-      </div>
-    );
-  }
-}
+const responseGoogle = (response) => {
+  console.log(response);
+  // const login = new SocialLogin();
+  // login.run(response.profileObj.givenName, response.profileObj.familyName, response.profileObj.email);
+};
+const Google = props => (
+  <div>
+    <script src="https://apis.google.com/js/platform.js" async defer />
+    <GoogleLogin
+      clientId={`${config.googleKey}`}
+      className={[styles.loginButton, styles.googleButton].join(' ')}
+      onSuccess={responseGoogle}
+    />
+    <script src={console.log(props)} /> {/* I'm trying to get these props up into responseGoogle, I can't just pass them into responseGoogle as an argument beacuse that breaks the response from google's api */}
+  </div>
+);
 
 export default Google;
