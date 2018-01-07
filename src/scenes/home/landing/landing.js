fcedc109516e9f21202b54e7c8b4116d19ac05f9
Tony Skuse <tskuse@gmail.com>
2018-01-07 14:19:34 -0800

    hide mailing list signup until Sidekiq/SendGrid issues are resolved #787

--- src/scenes/home/landing/landing.js
+++ src/scenes/home/landing/landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import ReactModal from 'react-modal';
 import { FaClose } from 'react-icons/lib/fa';
 import LinkButton from 'shared/components/linkButton/linkButton';
@@ -10,7 +10,7 @@ import SuccessStories from './successStories/successStories';
 import Partners from './partners/partners';
 import Donate from '../../../shared/components/donate/donate';
 import Join from '../../../shared/components/join/join';
-import EmailSignup from './emailSignup/emailSignup';
+// import EmailSignup from './emailSignup/emailSignup';
 
 import styles from './landing.css';
 
@@ -69,14 +69,14 @@ class Landing extends Component {
         <Partners />
         <Donate />
         <Join />
-        <EmailSignup sendNotification={this.props.sendNotification} />
+        {/* <EmailSignup sendNotification={this.props.sendNotification} /> */}
       </div>
     );
   }
 }
 
-Landing.propTypes = {
+/* Landing.propTypes = {
   sendNotification: PropTypes.func.isRequired
-};
+}; */
 
 export default Landing;
