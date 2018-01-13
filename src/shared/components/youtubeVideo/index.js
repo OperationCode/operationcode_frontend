1244740e96b25d8db04d04deab642e056f7a64ef
Katia <katia.amir@gmail.com>
2018-01-13 17:12:48 -0500

    add a shared youtube video component

---
+++ src/shared/components/youtubeVideo/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+import PropTypes from 'prop-types';
+
+const YoutubeVideo = ({ id, height, width }) => {
+  const options = {
+    height: `${height}`,
+    width: `${width}`,
+  };
+  return (
+    <YouTube
+      videoId={id}
+      opts={options}
+      onReady={(e) => { e.target.pauseVideo(); }}
+    />
+  );
+};
+
+YoutubeVideo.propTypes = {
+  id: PropTypes.string,
+  height: PropTypes.string,
+  width: PropTypes.string,
+};
+
+YoutubeVideo.defaultProps = {
+  id: '',
+  height: '390',
+  width: '640',
+};
+
+export default YoutubeVideo;
