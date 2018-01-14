b4194e6b17049e89c6bf5044df91f84aba5d80e8
Kyle Holmberg <kylemh.email@gmail.com>
2018-01-14 14:16:05 -0800

    Linting fixes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import { Route, Router } from 'react-router';
+import ReactGA from 'react-ga';
 import createHistory from 'history/createBrowserHistory';
 import ScrollToTop from 'shared/components/scrollToTop/scrollToTop';
-import withTracker from './scenes/withTracker';
 import Home from './scenes/home/home';
 
 const history = createHistory();
+ReactGA.initialize('UA-75642413-1');
 
 class App extends Component {
+  componentDidMount() {
+    if (process.env.NODE_ENV === 'production') {
+      history.listen((location) => {
+        ReactGA.set({ page: location.pathname });
+        ReactGA.pageview(location.pathname);
+      });
+    }
+  }
+
   render() {
     return (
       <Router history={history}>
         <ScrollToTop>
-          <Route
-            path="/"
-            component={// process.env.NODE_ENV === 'production' ? withTracker(Home) : Home
-            withTracker(Home)}
-          />
+          <Route path="/" component={Home} />
         </ScrollToTop>
       </Router>
     );
