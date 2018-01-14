d973ce87f92d424c9b8b209445811e7901eb289b
Kyle Holmberg <kylemh.email@gmail.com>
2018-01-14 13:29:09 -0800

    Fixes to content to prevent client-facing error statements

--- src/scenes/home/landing/membership/membership.js
+++ src/scenes/home/landing/membership/membership.js
@@ -7,37 +7,46 @@ const Membership = () => (
   <Section title="Membership" theme="white">
     <div className={styles.intro}>
       <p>
-        Joining Op-Code is Free. If you are just starting out, a senior dev, just exploring tech, or miss your buddies, Operation Code is for you.
+        Joining Op-Code is Free. If you are just starting out, a senior dev,
+        just exploring tech, or miss your buddies, Operation Code is for you.
       </p>
     </div>
     <br />
     <ul className={styles.list}>
       <li>
         <IconCard
-          title="Community" fontAwesomeIcon="FaGroup" iconAboveHeading
+          title="Community"
+          fontAwesomeIcon="FaGroup"
+          iconAboveHeading
           subText="Join our open community. Learn and grow with fellow veterans, servicemembers,
-          and military spouses. Work with your peers to build open source projects."
+          and military spouses."
         />
       </li>
       <li>
         <IconCard
-          title="Growth" fontAwesomeIcon="FaRoad" iconAboveHeading
+          title="Growth"
+          fontAwesomeIcon="FaRoad"
+          iconAboveHeading
           subText="Find new opportunities in the tech industry through our scholarship program.
-          Attend software development conferences and coding bootcamps, and build the skills of the future."
+          Attend conferences and coding bootcamps to build your skills."
         />
       </li>
       <li>
         <IconCard
-          title="Connections" fontAwesomeIcon="FaFlagCheckered" iconAboveHeading
-          subText="Jumpstart your journey. Access our mentorship and coding channels, and learn from senior
-          software engineers at top-tier tech companies with years of experience."
+          title="Connections"
+          fontAwesomeIcon="FaFlagCheckered"
+          iconAboveHeading
+          subText="Jump-start your journey. Gain access to our Slack team to learn from professional
+          software engineers with years of experience."
         />
       </li>
       <li>
         <IconCard
-          title="Careers" fontAwesomeIcon="FaSuitcase" iconAboveHeading
-          subText="Launch your career in the tech industry on our job board. Find local and remote jobs all
-          across the country. Get expert feedback on your portfolio, side project, or resume."
+          title="Careers"
+          fontAwesomeIcon="FaSuitcase"
+          iconAboveHeading
+          subText="Get expert feedback on your portfolio, side project, or resume. Find local and remote jobs all
+          across the country."
         />
       </li>
     </ul>
