1dbb97b0a16dbc109a17aad47394003f748871b9
Kyle Holmberg <kylemh.email@gmail.com>
2018-01-14 17:04:31 -0800

    Connect GA to partners section and make it easier to edit

---
+++ src/scenes/home/landing/partners/partnerRoster.js
@@ -0,0 +1,54 @@
+import apexLogo from 'images/partnerLogos/apex_systems_logo.png';
+import githubLogo from 'images/partnerLogos/github_logo.png';
+import dockerLogo from 'images/partnerLogos/docker_logo.png';
+import oracleLogo from 'images/partnerLogos/oracle_logo.png';
+import oreillyLogo from 'images/partnerLogos/oreilly_logo.png';
+import hackerrankLogo from 'images/partnerLogos/hackerrank_logo.png';
+import zapierLogo from 'images/partnerLogos/zapier_logo.png';
+
+const partners = [
+  {
+    name: 'APEX Systems',
+    order: 0,
+    link: 'https://apexsystems.com',
+    imgSource: apexLogo,
+  },
+  {
+    name: 'GitHub',
+    order: 1,
+    link: 'https://github.com',
+    imgSource: githubLogo,
+  },
+  {
+    name: 'Docker',
+    order: 2,
+    link: 'https://docker.com',
+    imgSource: dockerLogo,
+  },
+  {
+    name: 'Oracle',
+    order: 3,
+    link: 'https://oracle.com',
+    imgSource: oracleLogo,
+  },
+  {
+    name: "O'reilly Media",
+    order: 4,
+    link: 'https://oreilly.com',
+    imgSource: oreillyLogo,
+  },
+  {
+    name: 'HackerRank',
+    order: 5,
+    link: 'https://hackerrank.com',
+    imgSource: hackerrankLogo,
+  },
+  {
+    name: 'Zapier',
+    order: 6,
+    link: 'https://zapier.com',
+    imgSource: zapierLogo,
+  },
+];
+
+export default partners;
--- src/scenes/home/landing/partners/partners.js
+++ src/scenes/home/landing/partners/partners.js
@@ -1,74 +1,37 @@
 import React from 'react';
 import Section from 'shared/components/section/section';
 import LinkButton from 'shared/components/linkButton/linkButton';
-import apexLogo from 'images/partnerLogos/apex_systems_logo.png';
-import githubLogo from 'images/partnerLogos/github_logo.png';
-import dockerLogo from 'images/partnerLogos/docker_logo.png';
-import oracleLogo from 'images/partnerLogos/oracle_logo.png';
-import oreillyLogo from 'images/partnerLogos/oreilly_logo.png';
-import hackerrankLogo from 'images/partnerLogos/hackerrank_logo.png';
-import zapierLogo from 'images/partnerLogos/zapier_logo.png';
+import OutboundLink from 'shared/components/outboundLink/outboundLink';
+import partners from './partnerRoster';
 import styles from './partners.css';
 
 const Partners = () => (
-  <Section
-    title="Partners"
-    theme="gray"
-  >
+  <Section title="Partners" theme="gray">
     <div className={styles.partners}>
       <p>
-        Operation Code and our all-volunteer team offers our
-        programs and services through the generous support
-        of our partners. Their support, made via either
-        financial contributions or in-kind donations,
-        demonstrate their commitment to veterans,
-        servicemembers, and military spouses.
-        We are deeply grateful to the leadership of the
-        organizations that are supporting our mission.
+        Operation Code and our all-volunteer team offers our programs and
+        services through the generous support of our partners. Their support,
+        made via either financial contributions or in-kind donations,
+        demonstrate their commitment to veterans, servicemembers, and military
+        spouses. We are deeply grateful to the leadership of the organizations
+        that are supporting our mission.
       </p>
     </div>
 
     <div className={styles.partnerLogos}>
-      <div>
-        <a href="https://apexsystems.com/" target="_blank" rel="noopener noreferrer">
-          <img src={apexLogo} alt="Apex Systems" />
-        </a>
-      </div>
-      <div>
-        <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
-          <img src={githubLogo} alt="GitHub" />
-        </a>
-      </div>
-      <div>
-        <a href="https://docker.com/" target="_blank" rel="noopener noreferrer">
-          <img src={dockerLogo} alt="Docker" />
-        </a>
-      </div>
-      <div>
-        <a href="https://oreilly.com/" target="_blank" rel="noopener noreferrer">
-          <img src={oreillyLogo} alt="O&apos;Reilly Media" />
-        </a>
-      </div>
-      <div>
-        <a href="https://hackerrank.com/" target="_blank" rel="noopener noreferrer">
-          <img src={hackerrankLogo} alt="HackerRank" />
-        </a>
-      </div>
-      <div>
-        <a href="https://oracle.com/" target="_blank" rel="noopener noreferrer">
-          <img src={oracleLogo} alt="Oracle" />
-        </a>
-      </div>
-      <div>
-        <a href="https://www.zapier.com/" target="_blank" rel="noopener noreferrer">
-          <img src={zapierLogo} alt="Zapier" />
-        </a>
-      </div>
+      {partners.map(({ name, order, link, imgSource }) => (
+        <div key={order}>
+          <OutboundLink href={link} analyticsEventLabel={`[Partner] ${name}`}>
+            <img src={imgSource} alt={name} />
+          </OutboundLink>
+        </div>
+      ))}
     </div>
 
     <div className={styles.partners}>
       <p className={styles.contactUs}>
-        We are always looking for organizations who are interested in working with us!
+        We are always looking for organizations who are interested in working
+        with us!
       </p>
       <LinkButton
         text="Contact Us"
