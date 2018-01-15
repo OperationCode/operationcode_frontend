de67d3070bf809705892b99eac1abcdf76da7e09
wimo7 <wimo7083@colorado.edu>
2018-01-14 17:52:25 -0700

    changed conditional rendering

--- src/scenes/home/codeSchools/codeSchools.js
+++ src/scenes/home/codeSchools/codeSchools.js
@@ -12,8 +12,7 @@ class CodeSchools extends Component {
     super(props);
 
     this.state = {
-      schools: null,
-      validResponse: true
+      schools: null
     };
   }
 
@@ -25,85 +24,65 @@ class CodeSchools extends Component {
             schools: data
           });
         }))
-      .catch(() => {
-        this.setState({
-          schools: undefined,
-          validResponse: false
-        });
-      });
+      .catch(() => {});
   }
 
   render() {
-    if (this.state.validResponse) {
-      return (
-        <div>
-          <Section title="Code Schools" theme="white">
-            <p>
-              Code schools are accelerated learning programs that will prepare
-              you for a career in software development. <br />Each school listed
-              below ranges in length, vary in tuition costs, and in programming
-              languages. <br />Desirable from an employer&apos;s standpoint,
-              code schools are founded by software developers who saw a need{' '}
-              <br />for more programmers and aspired to teach the next
-              generation.
-              <br />
-              <br />
-              We encourage you to check out the schools below, do your research,
-              and ask fellow software developers in Slack in our Slack
-              Community.
-            </p>
-
-            <div className={styles.filterButtonDiv}>
-              <LinkButton
-                link="approvedSchools"
-                text="VA-Approved Schools"
-                theme="blue"
-                scrollLink
-              />
-              <LinkButton
-                link="partnerSchools"
-                text="Partnered Schools"
-                theme="blue"
-                scrollLink
-              />
-              <LinkButton
-                link="onlineSchools"
-                text="Online Schools"
-                theme="blue"
-                scrollLink
-              />
-              <LinkButton
-                link="schoolsByState"
-                text="Search by State"
-                theme="blue"
-                scrollLink
-              />
-            </div>
-          </Section>
-
-          {this.state.schools && (
-            <ApprovedSchools schools={this.state.schools} />
-          )}
-          {this.state.schools && (
-            <PartnerSchools schools={this.state.schools} />
-          )}
-          {this.state.schools && <OnlineSchools schools={this.state.schools} />}
-          {this.state.schools && (
-            <StateSortedSchools schools={this.state.schools} />
-          )}
-        </div>
-      );
-    }
-
     return (
       <div>
         <Section title="Code Schools" theme="white">
+          <p>
+            Code schools are accelerated learning programs that will prepare you
+            for a career in software development. <br />Each school listed below
+            ranges in length, vary in tuition costs, and in programming
+            languages. <br />Desirable from an employer&apos;s standpoint, code
+            schools are founded by software developers who saw a need <br />for
+            more programmers and aspired to teach the next generation.
+            <br />
+            <br />
+            We encourage you to check out the schools below, do your research,
+            and ask fellow software developers in Slack in our Slack Community.
+          </p>
+
+          <div className={styles.filterButtonDiv}>
+            <LinkButton
+              link="approvedSchools"
+              text="VA-Approved Schools"
+              theme="blue"
+              scrollLink
+            />
+            <LinkButton
+              link="partnerSchools"
+              text="Partnered Schools"
+              theme="blue"
+              scrollLink
+            />
+            <LinkButton
+              link="onlineSchools"
+              text="Online Schools"
+              theme="blue"
+              scrollLink
+            />
+            <LinkButton
+              link="schoolsByState"
+              text="Search by State"
+              theme="blue"
+              scrollLink
+            />
+          </div>
+        </Section>
+        {!this.state.schools && (
           <p className={styles.codeschoolError}>
             Whoops! Something went wrong… please check back later.
             <br />
           </p>
-          <p>In the mean time check out our Slack Community.</p>
-        </Section>
+        )}
+        {this.state.schools && <ApprovedSchools schools={this.state.schools} />}
+        {this.state.schools && <PartnerSchools schools={this.state.schools} />}
+        {this.state.schools && <OnlineSchools schools={this.state.schools} />}
+        {this.state.schools && (
+          <StateSortedSchools schools={this.state.schools} />
+        )}
       </div>
     );
   }
