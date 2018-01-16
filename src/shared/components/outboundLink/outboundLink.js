8d1805e065490ebc0f754d637b688404e9275a1f
Kyle Holmberg <kylemh.email@gmail.com>
2018-01-15 23:01:40 -0800

    Attach analytics to school cards 🎉

--- src/shared/components/outboundLink/outboundLink.js
+++ src/shared/components/outboundLink/outboundLink.js
@@ -2,7 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactGA from 'react-ga';
 
-const OutboundLink = ({ href, analyticsEventLabel, children }) => {
+const OutboundLink = ({
+  href,
+  analyticsEventLabel,
+  children,
+  className,
+  style,
+}) => {
   if (process.env.NODE_ENV === 'production') {
     // https://operationcode.org is 25 characters - only show what follows
     const location = window.location.href.slice(25);
@@ -13,6 +19,8 @@ const OutboundLink = ({ href, analyticsEventLabel, children }) => {
         eventLabel={`OUTBOUND [${analyticsEventLabel}] from ${location}`}
         target="_blank"
         rel="noopener noreferrer"
+        className={className}
+        style={style}
       >
         {children}
       </ReactGA.OutboundLink>
@@ -20,7 +28,13 @@ const OutboundLink = ({ href, analyticsEventLabel, children }) => {
   }
 
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer">
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={className}
+      style={style}
+    >
       {children}
     </a>
   );
@@ -30,6 +44,12 @@ OutboundLink.propTypes = {
   href: PropTypes.string.isRequired,
   analyticsEventLabel: PropTypes.string.isRequired,
   children: PropTypes.object.isRequired, // eslint-disable-line
+  className: PropTypes.string,
+  style: PropTypes.object, //eslint-disable-line
+};
+
+OutboundLink.defaultProps = {
+  className: '',
 };
 
 export default OutboundLink;
