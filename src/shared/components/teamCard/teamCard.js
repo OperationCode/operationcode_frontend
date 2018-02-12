74e00a3ad68387d454d623b3ec64cc5c83cfcb9f
Kyle Holmberg <inbox@kylemh.com>
2018-02-11 19:42:59 -0800

    Change variable name src to imageSrc

--- src/shared/components/teamCard/teamCard.js
+++ src/shared/components/teamCard/teamCard.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './teamCard.css';
 
-const TeamCard = ({ name, role, slack, email, isBoard, description, src }) => (
+const TeamCard = ({ name, role, slack, email, isBoard, description, imageSrc }) => (
   <div className={styles.teamCard}>
-    {src && (<img src={src} alt={`Headshot of ${name}`} />)}
+    {imageSrc && (<img src={imageSrc} alt={`Headshot of ${name}`} />)}
     <h6 className={styles.name}>{name}</h6>
     <i className={styles.role}>{role}</i>
     <hr className={styles.hr} />
@@ -32,14 +32,14 @@ TeamCard.propTypes = {
   slack: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
   isBoard: PropTypes.bool,
-  src: PropTypes.string,
+  imageSrc: PropTypes.string,
   description: PropTypes.string,
 };
 
 TeamCard.defaultProps = {
   description: null,
   isBoard: true,
-  src: ''
+  imageSrc: ''
 };
 
 export default TeamCard;
