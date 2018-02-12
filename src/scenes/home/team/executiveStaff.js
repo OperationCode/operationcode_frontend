8f3feb9a47491423a44b1163b5de4a13e0a471c0
Kyle Holmberg <inbox@kylemh.com>
2018-02-12 02:24:56 -0800

    Change values and variable name for Slack username

--- src/scenes/home/team/executiveStaff.js
+++ src/scenes/home/team/executiveStaff.js
@@ -1,70 +1,70 @@
 const CEO = {
   name: 'David Molina',
   role: 'CEO',
-  slack: '@david',
+  slackUsername: '@david',
   email: 'david@operationcode.org',
 };
 
 const ExecutiveAssistant = {
   name: 'Morgan Larrouy-Smith',
   role: 'Executive Assistant',
-  slack: '',
+  slackUsername: '',
   email: 'morgan@operationcode.org',
 };
 
 const COO = {
   name: 'David Reis',
   role: 'Acting COO',
-  slack: '@davidr',
+  slackUsername: '@davidr',
   email: '',
 };
 
 const CCMO = {
   name: 'Jennifer Weiderman',
   role: 'CCMO',
-  slack: '@Jenn',
+  slackUsername: '@jenn',
   email: 'jennifer@operationcode.org',
 };
 
 const CFO = {
   name: 'Amy Tan',
   role: 'CFO',
-  slack: '@amy Tan',
+  slackUsername: '@amytan',
   email: 'amy@operationcode.org',
 };
 
 const CTO = {
   name: 'Nell Shamrell',
   role: 'CTO',
-  slack: '@nellshamrell',
+  slackUsername: '@nellshamrell',
   email: 'nell@operationcode.org',
 };
 
 const CDO = {
   name: 'Kate Horner',
   role: 'Acting CDO',
-  slack: '@Katehorner',
+  slackUsername: '@katehorner',
   email: '',
 };
 
 const HR = {
   name: 'Kelly MacLeod',
   role: 'HR',
-  slack: '@kelly',
+  slackUsername: '@kelly',
   email: 'kelly@operationcode.org',
 };
 
 const PublicPolicyDirector = {
   name: 'Jameel Martin',
   role: 'Public Policy Director',
-  slack: '@0311-code',
+  slackUsername: '@0311-code',
   email: 'jameel@operationcode.org',
 };
 
 const GeneralCounsel = {
   name: 'Stefano DAniello',
   role: 'General Counsel',
-  slack: '@stefano',
+  slackUsername: '@stefano',
   email: '',
 };
 
