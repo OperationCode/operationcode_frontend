a3cc0a6ec6b170939fcff1844e0efd960d757fee
wimo7083 <wimo7083@colorado.edu>
2018-02-28 12:07:26 -0700

    prettier

--- src/scenes/home/landing/triGroup/triGroup.js
+++ src/scenes/home/landing/triGroup/triGroup.js
@@ -1,36 +1,46 @@
 import React from 'react';
-import smallRedLogo from 'images/logos/large-stacked-logo-blue.png';
-import smallBlueLogo from 'images/logos/large-stacked-logo-red.png';
-import imageStyles from '../../branding/logos/logos.css';
+import PropTypes from 'prop-types';
+import LinkButton from 'shared/components/linkButton/linkButton';
+import blueMedalLogo from 'images/logos/small-blue-medal.png';
+import redMedalLogo from 'images/logos/small-red-medal.png';
 import styles from './triGroup.css';
+import staticData from './triGroup.json';
 
-function TriGroup() {
-  return (
-    <div className="triGroupContainer">
-      <a href="/join">
-        <div className={imageStyles.smallLogos}>
-          <img src={smallRedLogo} alt="Stacked Small Slate Logo" />
-          <p>Join Us!</p>
-        </div>
-      </a>
-      <div className={styles.videos}>
-        <iframe
-          title="Watch Will battle react for hours"
-          src="https://www.youtube.com/embed/xN7yMoe38xc?modestbranding=1&start=133&showinfo=0"
-          frameBorder="0"
-          allowFullScreen
-          width="600"
-          height="340"
-        />
+const ClickableLogo = props => (
+  <div className={styles.clickableLogo}>
+    <a href={props.url}>
+      <div className={styles.medalLogos}>
+        <img src={props.logo} alt="Colored Medal Logo" />
+        <LinkButton link={props.url} text={props.text} theme={props.color} scrollLink />
       </div>
-      <a href="https://opencollective.com/operationcode#support">
-        <div className={imageStyles.smallLogos}>
-          <img src={smallBlueLogo} alt="Stacked Small Slate Logo" />
-          <p>Donate Now!</p>
-        </div>
-      </a>
+    </a>
+  </div>
+);
+
+ClickableLogo.propTypes = {
+  url: PropTypes.string.isRequired,
+  logo: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
+
+const TriGroup = () => (
+  <div className={styles.triGroupContainer}>
+    <ClickableLogo logo={redMedalLogo} {...staticData.join} />
+
+    <div className={styles.videos}>
+      <iframe
+        title={staticData.video.title}
+        src={staticData.video.url}
+        frameBorder="0"
+        allowFullScreen
+        width="600"
+        height="340"
+      />
     </div>
-  );
-}
+
+    <ClickableLogo logo={blueMedalLogo} {...staticData.donate} />
+  </div>
+);
 
 export default TriGroup;
