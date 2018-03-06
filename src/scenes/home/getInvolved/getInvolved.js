685751e2476529a454c9168476917b07593296e4
K MacLeod <ksmacleod99@gmail.com>
2018-03-06 09:39:11 -0500

    removed link button

--- src/scenes/home/getInvolved/getInvolved.js
+++ src/scenes/home/getInvolved/getInvolved.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import LinkButton from 'shared/components/linkButton/linkButton';
 import HeroBanner from 'shared/components/heroBanner/heroBanner';
 import banner1 from 'images/bannerimg1.jpg';
 import WhyGive from './whyGive/whyGive';
@@ -13,15 +12,7 @@ const getInvolved = () => (
       imageSrc={banner1}
       title="Get Involved"
       subtitle="See the magic happen..."
-    >
-      <LinkButton
-        text="Donate"
-        theme="red"
-        link="https://opencollective.com/operationcode#support"
-        isExternal
-      />
-      <LinkButton text="Volunteer" theme="blue" link="/signup" />
-    </HeroBanner>
+    />
 
     <WhyGive />
     <Volunteer />
