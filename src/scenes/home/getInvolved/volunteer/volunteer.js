05e904c84a174061270621458bb3fcefd15dca34
K MacLeod <ksmacleod99@gmail.com>
2018-03-06 10:44:16 -0500

    FA check
    adds FA check, removed check.svg file

--- src/scenes/home/getInvolved/volunteer/volunteer.js
+++ src/scenes/home/getInvolved/volunteer/volunteer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
 import LinkButton from 'shared/components/linkButton/linkButton';
 import hackathon1 from 'images/hackathon1.jpg';
 import styles from './volunteer.css';
@@ -8,25 +10,24 @@ const Volunteer = () => (
     <div className={styles.wrapper}>
       <div className={styles.half}>
         <h3>Become A Volunteer</h3>
-        <ul>
-          <li className={styles.check}>
-            Join our community on Slack, and work with new software developers
-            to help grow and mentor them in learning to code and enter the tech
-            industry. Answer questions, participate in mock interviews, and
-            review resumes.
-          </li>
-          <br />
-          <li className={styles.check}>
-            Lead a squad of new developers in learning new skills in Linux,
-            Python, cloud infrastructure, web development, and more.
-          </li>
-          <br />
-          <li className={styles.check}>
-            Meet up with a local chapter (or start your own!) and network with
-            other new developers eager to build cool apps and code the future!
-          </li>
-          <br />
-        </ul>
+        <p>
+          <FontAwesomeIcon icon={faCheck} size="2x" />
+          Join our community on Slack, and work with new software developers to
+          help grow and mentor them in learning to code and enter the tech
+          industry. Answer questions, participate in mock interviews, and review
+          resumes.
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faCheck} size="2x" />
+          Lead a squad of new developers in learning new skills in Linux,
+          Python, cloud infrastructure, web development, and more.
+        </p>
+
+        <p>
+          <FontAwesomeIcon icon={faCheck} size="2x" />
+          Meet up with a local chapter (or start your own!) and network with
+          other new developers eager to build cool apps and code the future!
+        </p>
         <div className={styles.button}>
           <LinkButton text="Volunteer" theme="red" link="/signup" />
         </div>
--- src/scenes/home/getInvolved/whyGive/whyGive.js
+++ src/scenes/home/getInvolved/whyGive/whyGive.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
 import LinkButton from 'shared/components/linkButton/linkButton';
 import Dinner1 from 'images/benefit_dinner_2017_1.jpg';
 import styles from './whyGive.css';
@@ -10,27 +12,31 @@ const WhyGive = () => (
     </div>
     <div className={styles.half}>
       <h3>Why give to Operation Code?</h3>
-      <ul>
-        <li className={styles.check}>
-          To provide veterans, transitioning servicemembers, and military
-          spouses with transformative educational experiences
-        </li>
-        <li className={styles.check}>
-          To advocate for more diversity and opportunity in the tech industry
-        </li>
-        <li className={styles.check}>
-          To enable our mission to reach more members of the military community
-        </li>
-        <li className={styles.check}>
-          Scholarship funding for comp sci education, industry certification
-        </li>
-        <li className={styles.check}>
-          Sending veterans to conferences and major tech industry events
-        </li>
-        <li className={styles.check}>
-          Building more tech communities near military installations
-        </li>
-      </ul>
+      <p>
+        <FontAwesomeIcon icon={faCheck} size="2x" />
+        To provide veterans, transitioning servicemembers, and military spouses
+        with transformative educational experiences
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faCheck} size="2x" />
+        To advocate for more diversity and opportunity in the tech industry
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faCheck} size="2x" />
+        To enable our mission to reach more members of the military community
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faCheck} size="2x" />
+        Scholarship funding for comp sci education, industry certification
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faCheck} size="2x" />
+        Sending veterans to conferences and major tech industry events
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faCheck} size="2x" />
+        Building more tech communities near military installations
+      </p>
       <div className={styles.button}>
         <LinkButton
           text="Donate"
