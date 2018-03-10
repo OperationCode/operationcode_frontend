5b43610d207a3cd1d044d7319d33bba8410fabce
Kyle Holmberg <inbox@kylemh.com>
2018-03-10 10:40:31 -0800

    Re-organize TriGroup.js

--- src/scenes/home/landing/triGroup/triGroup.js
+++ src/scenes/home/landing/triGroup/triGroup.js
@@ -1,36 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import LinkButton from 'shared/components/linkButton/linkButton';
+import OutboundLink from 'shared/components/outboundLink/outboundLink';
 import blueMedalLogo from 'images/logos/small-blue-medal.png';
 import redMedalLogo from 'images/logos/small-red-medal.png';
 import styles from './triGroup.css';
-import staticData from './triGroup.json';
 
 const TriGroup = () => {
-  const ClickableLogo = ({ color, isExternal, logo, text, url }) => (
-    <div className={styles.clickableLogo}>
-      <a href={url}>
-        <img src={logo} alt="Colored Medal Logo" />
-      </a>
+  const ClickableLogo = ({ color, isExternal, logo, text, url }) => {
+    ClickableLogo.propTypes = {
+      color: PropTypes.string.isRequired,
+      isExternal: PropTypes.bool,
+      logo: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    };
 
-      <LinkButton
-        link={url}
-        text={text}
-        theme={color}
-        isExternal={isExternal}
-      />
-    </div>
-  );
+    ClickableLogo.defaultProps = {
+      isExternal: false,
+    };
+
+    return (
+      <div className={styles.clickableLogo}>
+        <OutboundLink href={url}>
+          <img src={logo} alt="Colored Medal Logo" />
+        </OutboundLink>
+
+        <LinkButton link={url} text={text} theme={color} isExternal={isExternal} />
+      </div>
+    );
+  };
 
   return (
     <div className={styles.triGroupContainer}>
-      <ClickableLogo logo={redMedalLogo} {...staticData.join} />
+      <ClickableLogo color="red" logo={redMedalLogo} text="Join Us!" url="/join" />
 
       <div className={styles.videos}>
         <iframe
           className={styles.triGroupVideo}
-          title={staticData.video.title}
-          src={staticData.video.url}
+          title="The New Developer - Operation Code - GitHub Universe 2016"
+          src="https://www.youtube.com/embed/xN7yMoe38xc?modestbranding=1&start=133&showinfo=0"
           frameBorder="0"
           allowFullScreen
           width="800"
@@ -38,7 +47,13 @@ const TriGroup = () => {
         />
       </div>
 
-      <ClickableLogo logo={blueMedalLogo} {...staticData.donate} />
+      <ClickableLogo
+        color="blue"
+        isExternal
+        logo={blueMedalLogo}
+        text="Donate Now!"
+        url="https://opencollective.com/operationcode#support"
+      />
     </div>
   );
 };
