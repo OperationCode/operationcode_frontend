65ed421fc3440049a3dfd7219b1e48175ec8c5ce
K MacLeod <ksmacleod99@gmail.com>
2018-03-18 10:34:07 -0400

    adds animation
    includes new dependency

--- src/scenes/home/gala/gala.js
+++ src/scenes/home/gala/gala.js
@@ -1,33 +1,58 @@
 import React from 'react';
+import ReactRevealText from 'react-reveal-text';
 import commonUrl from 'shared/constants/commonLinks';
 import LinkButton from 'shared/components/linkButton/linkButton';
 import Styles from './gala.css';
 
-const Gala = () => (
-  <div className={Styles.hero}>
-    <div className={Styles.heading}>
-      <h1 className="mast__title js-spanize">Operation Code</h1>
-      <h2 className="mast__title js-spanize">Deploy the Future Gala 2018</h2>
-      <p>Saturday, November 10th, 2018</p>
-      <p>
-        <a
-          href="mailto:staff@operationcode.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Contact us
-        </a>{' '}
-        for sponsorship information.
-      </p>
-      <LinkButton
-        role="button"
-        text="Donate"
-        theme="blue"
-        link={commonUrl.donateLink}
-        isExternal
-      />
-    </div>
-  </div>
-);
+class Gala extends React.Component {
+  constructor() {
+    super();
+    this.state = { show: false };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: true });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className={Styles.hero}>
+        <div className={Styles.heading}>
+          <h1>
+            <ReactRevealText show={this.state.show}>
+              Operation Code
+            </ReactRevealText>
+          </h1>
+          <h2>
+            <ReactRevealText show={this.state.show}>
+              Deploy the Future Gala 2018
+            </ReactRevealText>
+          </h2>
+          <p>Saturday, November 10th, 2018</p>
+          <p>
+            <a
+              href="mailto:staff@operationcode.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Contact us
+            </a>{' '}
+            for sponsorship information.
+          </p>
+          <LinkButton
+            role="button"
+            text="Donate"
+            theme="blue"
+            link={commonUrl.donateLink}
+            isExternal
+          />
+          <p>Check back for more information</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Gala;
