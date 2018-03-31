eacc7101343d1727bfebffc21c142981f2b10e88
John Hampton <john_j_hampton@yahoo.com>
2018-03-31 18:10:09 -0400

    Add header content/styles

--- src/scenes/home/policy/policy.js
+++ src/scenes/home/policy/policy.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import styles from './policy.css';
 
 const Policy = () => (
-  <div>
-    {' '}
+  <div className={styles.content}>
+    <h1 className={styles.header}>
+      <div className={styles.headerTitle}>Modernize the GI Bill</div>
+      <div className={styles.headerSubtitle}>
+        We&apos;re heading to Washington, D.C. to demand that Congress develop
+        policies to make veterans more competitive for careers in the tech
+        sector.
+      </div>
+    </h1>
   </div>
 );
 
