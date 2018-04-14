4e878503a1c62765a7b3f237501e7e71f54d26ae
John Hampton <john_j_hampton@yahoo.com>
2018-04-14 14:44:05 -0400

    Clean up markup/CSS for whyGive, use Section component
    Added mobile-friendly styles
    Added whitespace btw/ checkmark and text

--- src/scenes/home/getInvolved/whyGive/whyGive.js
+++ src/scenes/home/getInvolved/whyGive/whyGive.js
@@ -2,51 +2,74 @@ import React from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
 import LinkButton from 'shared/components/linkButton/linkButton';
+import Section from 'shared/components/section/section';
 import Dinner1 from 'images/benefit_dinner_2017_1.jpg';
 import styles from './whyGive.css';
 
 const WhyGive = () => (
-  <div className={styles.wrapper}>
-    <div className={styles.half}>
-      <img src={Dinner1} alt="Benefit Dinner 2017" className={styles.photo} />
-    </div>
-    <div className={styles.half}>
-      <h3>Why give to Operation Code?</h3>
-      <p>
-        <FontAwesomeIcon icon={faCheck} size="2x" />
-        To provide veterans, transitioning servicemembers, and military spouses
-        with transformative educational experiences
-      </p>
-      <p>
-        <FontAwesomeIcon icon={faCheck} size="2x" />
-        To advocate for more diversity and opportunity in the tech industry
-      </p>
-      <p>
-        <FontAwesomeIcon icon={faCheck} size="2x" />
-        To enable our mission to reach more members of the military community
-      </p>
-      <p>
-        <FontAwesomeIcon icon={faCheck} size="2x" />
-        Scholarship funding for comp sci education, industry certification
-      </p>
-      <p>
-        <FontAwesomeIcon icon={faCheck} size="2x" />
-        Sending veterans to conferences and major tech industry events
-      </p>
-      <p>
-        <FontAwesomeIcon icon={faCheck} size="2x" />
-        Building more tech communities near military installations
-      </p>
-      <div className={styles.button}>
-        <LinkButton
-          text="Donate"
-          theme="red"
-          link="https://opencollective.com/operationcode#support"
-          isExternal
-        />
+  <Section title="Why give to Operation Code?" theme="white">
+    <div className={styles.wrapper}>
+      <div className={styles.half}>
+        <img src={Dinner1} alt="Benefit Dinner 2017" className={styles.photo} />
+      </div>
+      <div className={styles.half}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+            <FontAwesomeIcon icon={faCheck} size="2x" />
+            <p className={styles.itemText}>
+              To provide veterans, transitioning servicemembers, and military
+              spouses with transformative educational experiences
+            </p>
+          </li>
+
+          <li className={styles.listItem}>
+            <FontAwesomeIcon icon={faCheck} size="2x" />
+            <p className={styles.itemText}>
+              To advocate for more diversity and opportunity in the tech
+              industry
+            </p>
+          </li>
+
+          <li className={styles.listItem}>
+            <FontAwesomeIcon icon={faCheck} size="2x" />
+            <p className={styles.itemText}>
+              To enable our mission to reach more members of the military
+              community
+            </p>
+          </li>
+
+          <li className={styles.listItem}>
+            <FontAwesomeIcon icon={faCheck} size="2x" />
+            <p className={styles.itemText}>
+              Scholarship funding for comp sci education, industry certification
+            </p>
+          </li>
+
+          <li className={styles.listItem}>
+            <FontAwesomeIcon icon={faCheck} size="2x" />
+            <p className={styles.itemText}>
+              Sending veterans to conferences and major tech industry events
+            </p>
+          </li>
+
+          <li className={styles.listItem}>
+            <FontAwesomeIcon icon={faCheck} size="2x" />
+            <p className={styles.itemText}>
+              Building more tech communities near military installations
+            </p>
+          </li>
+        </ul>
       </div>
     </div>
-  </div>
+    <div className={styles.button}>
+      <LinkButton
+        text="Donate"
+        theme="red"
+        link="https://opencollective.com/operationcode#support"
+        isExternal
+      />
+    </div>
+  </Section>
 );
 
 export default WhyGive;
