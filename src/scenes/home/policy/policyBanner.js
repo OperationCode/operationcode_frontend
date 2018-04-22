c999f8b169f44ef712cedfb6ec7550ee9cf0bea8
John Hampton <john_j_hampton@yahoo.com>
2018-04-22 11:32:23 -0400

    Comment out old text content in Policy / PolicyBanner components
    Leaving these components in the codebase, the text content can be updated if they need to be reused later.

--- src/scenes/home/policy/policyBanner.js
+++ src/scenes/home/policy/policyBanner.js
@@ -3,10 +3,12 @@ import styles from './policyBanner.css';
 
 const policyPageLink = '/policy';
 
+// replace commented out bannertext below with new text if this component is needed for reuse in the future
+
 const PolicyBanner = () => (
   <div className={styles.banner}>
     <h4 className={styles.bannerText}>
-      On April 19, 2018, we&apos;re heading to Washington, D.C. to modernize the G.I. Bill.{' '}
+      {/* On April 19, 2018, we&apos;re heading to Washington, D.C. to modernize the G.I. Bill.{' '} */}
       <a href={policyPageLink} className={styles.bannerLink}>
         Click here for details!
       </a>
