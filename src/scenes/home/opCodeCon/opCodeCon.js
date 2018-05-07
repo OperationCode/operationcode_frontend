33fe8dea54177f76d3bf8df6eaa95414a452378c
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2018-05-07 15:38:16 -0400

    revamped the con page

--- src/scenes/home/opCodeCon/opCodeCon.js
+++ src/scenes/home/opCodeCon/opCodeCon.js
@@ -6,21 +6,18 @@ import styles from './opCodeCon.css';
 const OpCodeCon = () => (
   <div className={styles.hero}>
     <div className={styles.heading}>
-      <h1>OpCodeCon</h1>
       <h3>Join us for our inaugural Operation Code Convention!</h3>
-      <p>September 19th-20th, 2018</p>
-      <p>Raleigh Convention Center, Raleigh, NC</p>
-      <p>
-        <a href="mailto:eilish@operationcode.org" className={styles.textLink}>
-          Contact us
-        </a>{' '}
-        for sponsorship information.
-      </p>
+      <h3>Contact us for sponsorship information or make a donation!</h3>
       <LinkButton
         role="button"
-        text="Donate"
+        text="Sponsor"
         theme="red"
-        link={commonUrl.donateLink}
+        link="mailto:elish@operationcode.org"
+        isExternal
+      />
+
+      <LinkButton
+        role="button" text="Donate" theme="red" link={commonUrl.donateLink}
         isExternal
       />
       {/* <p>Check back for more information.</p> */}
