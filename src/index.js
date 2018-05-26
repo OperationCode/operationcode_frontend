7336bfd5b655aacf6ce2392f15a272ca388d2db3
Seth Bergman <seth.atxwebs@gmail.com>
2018-05-25 20:56:32 -0500

    updated raven config
    On branch issue955
    Changes to be committed:
    - modified:   package.json
    - modified:   public/index.html
    - modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Raven from 'raven-js';
 import '@fortawesome/fontawesome/styles.css';
 import App from './App';
 import './shared/styles/normalize.global.css';
@@ -7,7 +8,6 @@ import './shared/styles/react-select.global.css';
 import './shared/styles/notifications.global.css';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+Raven.config('https://6350058c0b0247bf8923b9965ca744fa@sentry.io/222621').install();
+
+ReactDOM.render(<App />, document.getElementById('root'));
