a7833a5ad9769accd3f810d6379a07c1188f2b71
Kyle Holmberg <inbox@kylemh.com>
2018-06-17 10:32:44 -0700

    Make link HTTPS

--- src/scenes/home/codeSchools/codeSchools.js
+++ src/scenes/home/codeSchools/codeSchools.js
@@ -54,7 +54,7 @@ class CodeSchools extends Component {
             <br />
             <strong>Code Schools:</strong> if you&apos;d like your school to be listed here, please
             fill out our{' '}
-            <OutboundLink href="http://op.co.de/code-school-request">request form</OutboundLink>.
+            <OutboundLink href="https://op.co.de/code-school-request">request form</OutboundLink>.
           </p>
 
           <div className={styles.filterButtonDiv}>
