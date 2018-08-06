08e48cc5c30855c7d19de9ea3881e036c338cf89
Johnny Villegas <cyph3r.m00n@gmail.com>
2018-08-06 13:44:53 -0400

    Changed Anchor Tags to use the OutboundLink component for Analytics.

--- src/scenes/home/press/pressLinks/pressLinks.js
+++ src/scenes/home/press/pressLinks/pressLinks.js
@@ -1,180 +1,319 @@
 import React from 'react';
-import { a } from 'react-router-dom';
 import styles from './pressLinks.css';
+
+const ReactGA = require('react-ga');
 
 function PressLinks() {
   return (
     <div className={styles.logos}>
       <ul>
         <li>
-          <a href="https://blog.opportunityatwork.org/techhire-educator-spotlight-operation-code-debd0a796f9d">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://blog.opportunityatwork.org/techhire-educator-spotlight-operation-code-debd0a796f9d"
+            target="_blank"
+          >
             TechHire Educator Spotlight: Operation Code
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.switchup.org/blog/why-veterans-will-make-excellent-programmers">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.switchup.org/blog/why-veterans-will-make-excellent-programmers"
+            target="_blank"
+          >
             Why Veterans Will Make Excellent Programmers
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.wired.com/story/thousands-of-veterans-want-to-learn-to-code-but-cant/">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.wired.com/story/thousands-of-veterans-want-to-learn-to-code-but-cant/"
+            target="_blank"
+          >
             Thousands Of Veterans Want to Learn to Code But Cant
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://medium.com/the-hum/hacking-entrepreneurship-an-interview-with-david-molina-of-operation-code-12a7e199e4e0">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://medium.com/the-hum/hacking-entrepreneurship-an-interview-with-david-molina-of-operation-code-12a7e199e4e0"
+            target="_blank"
+          >
             Hacking Entrepreneurship: An Interview with David Molina of Operation Code
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://medium.com/operation-code/a-marine-vets-path-into-coding-brings-him-back-home-4fcf5645d740">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://medium.com/operation-code/a-marine-vets-path-into-coding-brings-him-back-home-4fcf5645d740"
+            target="_blank"
+          >
             A Marine Vet’s Path into Coding Brings Him Back Home
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="http://blog.teamtreehouse.com/operation-code-connecting-veterans-with-code">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="http://blog.teamtreehouse.com/operation-code-connecting-veterans-with-code"
+            target="_blank"
+          >
             Operation Code: Connecting Veterans with Code Skills
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://blog.github.com/2016-11-11-operation-code-connecting-tech-and-veterans/">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://blog.github.com/2016-11-11-operation-code-connecting-tech-and-veterans/"
+            target="_blank"
+          >
             Operation Code: connecting tech and veterans
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.geekwire.com/2016/call-duty-technology-veterans-rally-support-operation-code/">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.geekwire.com/2016/call-duty-technology-veterans-rally-support-operation-code/"
+            target="_blank"
+          >
             When the call of duty is technology, veterans rally to support each other through
             Operation Code
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.youtube.com/watch?v=qoy7scC2SHk">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.youtube.com/watch?v=qoy7scC2SHk"
+            target="_blank"
+          >
             Get Coding Now with Operation Code Army Veteran and Founder David Molina
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.youtube.com/watch?v=xN7yMoe38xc">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.youtube.com/watch?v=xN7yMoe38xc"
+            target="_blank"
+          >
             The New Developer - Operation Code - GitHub Universe 2016
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.youtube.com/watch?v=-wSwlLeKFdE">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.youtube.com/watch?v=-wSwlLeKFdE"
+            target="_blank"
+          >
             What happens when military veterans learn to code - CodeConf 2016
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://opensource.com/article/17/4/operation-code-mentors-veterans">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://opensource.com/article/17/4/operation-code-mentors-veterans"
+            target="_blank"
+          >
             How Operation Code helps veterans learn programming skills
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://softwareengineeringdaily.com/2016/03/20/helping-veterans-learn-code-david-molina/">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://softwareengineeringdaily.com/2016/03/20/helping-veterans-learn-code-david-molina/"
+            target="_blank"
+          >
             Helping Veterans Learn to Code with David Molina
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://developingstory.netlify.com/tds-66-david-molina-from-operation-code">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://developingstory.netlify.com/tds-66-david-molina-from-operation-code"
+            target="_blank"
+          >
             Podcast from David Molina - Operation Code
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.instagram.com/p/21p5bFxUjd/?taken-by=davidcmolina">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.instagram.com/p/21p5bFxUjd/?taken-by=davidcmolina"
+            target="_blank"
+          >
             Operation Code and AirBnb
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="http://blog.teamtreehouse.com/from-aviation-electrician-to-back-end-engineering-bret-funk-operation-code-story">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="http://blog.teamtreehouse.com/from-aviation-electrician-to-back-end-engineering-bret-funk-operation-code-story"
+            target="_blank"
+          >
             From Aviation Electrician to Back End Engineering: Bret Funk’s Operation Code story
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="http://blog.teamtreehouse.com/navy-veteran-to-software-developer-geno-guerreros-operation-code-story">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="http://blog.teamtreehouse.com/navy-veteran-to-software-developer-geno-guerreros-operation-code-story"
+            target="_blank"
+          >
             Navy Veteran to Software Developer: Geno Guerrero’s Operation Code Story
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="http://blog.teamtreehouse.com/marine-corps-veteran-front-end-developer-billy-le">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="http://blog.teamtreehouse.com/marine-corps-veteran-front-end-developer-billy-le"
+            target="_blank"
+          >
             From Marine Corps Veteran to Front End Developer: Billy Le’s Operation Code Story
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://techcrunch.com/2015/11/11/few-options-for-veterans-looking-to-enter-tech/">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://techcrunch.com/2015/11/11/few-options-for-veterans-looking-to-enter-tech/"
+            target="_blank"
+          >
             Few Options For Veterans Loking To Enter Tech
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.ibtimes.com/coding-boot-camps-go-after-veterans-take-silicon-valleys-vacant-tech-jobs-2174421">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.ibtimes.com/coding-boot-camps-go-after-veterans-take-silicon-valleys-vacant-tech-jobs-2174421"
+            target="_blank"
+          >
             Coding Boot Camps Go After Veterans To Take Silicon Valleys Vacant Tech Jobs
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.makeschool.com/blog/20-diversity-and-inclusion-leaders-to-follow-in-2018">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.makeschool.com/blog/20-diversity-and-inclusion-leaders-to-follow-in-2018"
+            target="_blank"
+          >
             20 Diversity and Inclusion Leaders to Follow in 2018
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="http://bitshare.cm/news/tech-innovation-meets-military-service-geekwires-memorial-day-remembrance-and-update/">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="http://bitshare.cm/news/tech-innovation-meets-military-service-geekwires-memorial-day-remembrance-and-update/"
+            target="_blank"
+          >
             Tech innovation meets military service: GeekWire’s Memorial Day remembrance and update
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.millennialaction.org/press-archives/vetstechpr">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.millennialaction.org/press-archives/vetstechpr"
+            target="_blank"
+          >
             MAP Hosts Congressional Briefing on Veterans Readiness in Tech Careers
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.advfn.com/news_Analytics-Pros-Inc-Hosts-Training-for-Veterans-a_72657464.html">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.advfn.com/news_Analytics-Pros-Inc-Hosts-Training-for-Veterans-a_72657464.html"
+            target="_blank"
+          >
             Analytics Pros, Inc. Hosts Training for Veterans and Veterans Spouses
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="http://mentoringdevelopers.com/episode-36-how-to-pick-a-programming-language-to-learn-for-new-developers-part-2/">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="http://mentoringdevelopers.com/episode-36-how-to-pick-a-programming-language-to-learn-for-new-developers-part-2/"
+            target="_blank"
+          >
             How to pick a programming language to learn for new developers
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.blogs.va.gov/VAntage/33028/upskill-veterans-training-technology-jobs/">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.blogs.va.gov/VAntage/33028/upskill-veterans-training-technology-jobs/"
+            target="_blank"
+          >
             How to “upskill” Veterans’ training for technology jobs
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.instagram.com/p/BbZT3Q-BBDC/">Oregon Veterans News Magazine</a>
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.instagram.com/p/BbZT3Q-BBDC/"
+            target="_blank"
+          >
+            Oregon Veterans News Magazine
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://blog.sabio.la/jameel-from-jarhead-to-softwareengineer-9130702da5c1">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://blog.sabio.la/jameel-from-jarhead-to-softwareengineer-9130702da5c1"
+            target="_blank"
+          >
             Jameel: From Marines to SoftwareEngineer
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.seattletimes.com/business/technology/seattles-code-fellows-wins-approval-under-gi-bill/">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.seattletimes.com/business/technology/seattles-code-fellows-wins-approval-under-gi-bill/"
+            target="_blank"
+          >
             Seattle coding-school tuition to be covered by GI Bill
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://www.koin.com/news/veteran-gi-bill-should-cover-code-school_20180208085618706/960235415">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://www.koin.com/news/veteran-gi-bill-should-cover-code-school_20180208085618706/960235415"
+            target="_blank"
+          >
             Veteran: GI Bill should cover code school
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="http://patriotbootcamp.org/blog/operation-code">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="http://patriotbootcamp.org/blog/operation-code"
+            target="_blank"
+          >
             Patriot Boot Camp and Operation Code join forces to help military veterans become
             technology entrepreneurs
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="http://www.maswired.com/operation-code-wants-veterans-to-work-in-tech/">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="http://www.maswired.com/operation-code-wants-veterans-to-work-in-tech/"
+            target="_blank"
+          >
             Operation Code wants veterans to work in tech
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="http://diversemilitary.net/2017/06/27/coding-bootcamps-accepts-gi-bill/">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="http://diversemilitary.net/2017/06/27/coding-bootcamps-accepts-gi-bill/"
+            target="_blank"
+          >
             Coding Bootcamps Accepts GI Bill
-          </a>
+          </ReactGA.OutboundLink>
         </li>
         <li>
-          <a href="https://news.clearancejobs.com/2017/07/17/operation-code-looks-help-veterans-land-careers/">
+          <ReactGA.OutboundLink
+            eventLabel="myLabel"
+            to="https://news.clearancejobs.com/2017/07/17/operation-code-looks-help-veterans-land-careers/"
+            target="_blank"
+          >
             Operation Code Looks to Help Veterans Land IT Careers
-          </a>
+          </ReactGA.OutboundLink>
         </li>
       </ul>
     </div>
