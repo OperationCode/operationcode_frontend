56b7b64ba06baa8b61c20d0e49a81e5011655590
Jaclyn Feminella <jaclynfeminella@gmail.com>
2018-08-06 20:02:29 -0400

    Use OutboundLink component for badge

--- src/scenes/home/press/civicXBadge/civicXBadge.js
+++ src/scenes/home/press/civicXBadge/civicXBadge.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import OutboundLink from 'shared/components/outboundLink/outboundLink';
 import civicX from '../../../../images/civic-x.png';
 import styles from './civicXBadge.css';
 
-const CivicXBadge = () => (
+const CivicXBadge = ({ sourceUrl = 'http://cvcx.org/veterans-solutions-lab/' }) => (
   <div>
-    <a href="http://cvcx.org/veterans-solutions-lab/">
+    <OutboundLink href={sourceUrl} analyticsEventLabel={`[CivicX Accelerator Badge] ${sourceUrl}`}>
       <img
         alt="Operation Code is a graduate of the CivicX Accelerator."
         className={styles.civic}
         src={civicX}
       />
-    </a>
+    </OutboundLink>
   </div>
 );
+
+CivicXBadge.propTypes = {
+  sourceUrl: PropTypes.string.isRequired
+};
 
 export default CivicXBadge;
