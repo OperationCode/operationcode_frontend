39a84bcfd3bf7e3e5123cead13391831d291a9da
Kyle Holmberg <inbox@kylemh.com>
2018-09-06 19:28:36 -0700

    Rename environment variables to be clear about values

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,7 +1,7 @@
 const config = {
-  backendHost: process.env.OC_BACKEND_HOST,
-  backendUrl: process.env.OC_BACKEND_URL,
-  host: process.env.OC_HOST,
+  backendHost: process.env.OC_BACKEND_URL,
+  apiUrl: process.env.OC_API_URL,
+  host: process.env.OC_HOST_URL,
   googleKey: process.env.OC_GOOGLE_KEY,
   facebookKey: process.env.OC_FACEBOOK_KEY
 };
