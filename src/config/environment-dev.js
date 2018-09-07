30ecc131835a9383367b9ef9d5daa89ebb45c856
Kyle Holmberg <inbox@kylemh.com>
2018-09-06 18:58:23 -0700

    Remove all references to IDME

--- src/config/environment-dev.js
+++ src/config/environment-dev.js
@@ -1,9 +1,7 @@
 const config = {
   backendHost: 'http://localhost:3000/',
   backendUrl: 'http://localhost:3000/api/v1',
-  idmeClientId: '6d781bfd42506613a0fe4ad4123aaf6d',
-  host: 'http://localhost:4000',
-  idmeOAuthUrl: 'http://localhost:4001/oauth/authorize'
+  host: 'http://localhost:4000'
 };
 
 export default config;
--- src/config/environment-prod.js
+++ src/config/environment-prod.js
@@ -1,9 +1,7 @@
 const config = {
   backendHost: 'https://api.operationcode.org/',
   backendUrl: 'https://api.operationcode.org/api/v1',
-  idmeClientId: '6d781bfd42506613a0fe4ad4123aaf6d',
-  host: 'https://www.operationcode.org',
-  idmeOAuthUrl: 'https://api.id.me/oauth/authorize'
+  host: 'https://www.operationcode.org'
 };
 
 export default config;
