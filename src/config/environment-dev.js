597423403105c104929175c59ea87a23fd96c058
Kyle Holmberg <inbox@kylemh.com>
2018-09-06 19:10:06 -0700

    Resolve merge conflicts

--- src/config/environment-dev.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  backendHost: 'http://localhost:3000/',
-  backendUrl: 'http://localhost:3000/api/v1',
-  idmeClientId: '6d781bfd42506613a0fe4ad4123aaf6d',
-  host: 'http://localhost:4000',
-  idmeOAuthUrl: 'http://localhost:4001/oauth/authorize'
-};
-
-export default config;
