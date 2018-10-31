0fe8ea94ffafcb09d2e9baedd8674451ae9af3c2
wimo7083 <wimo7083@colorado.edu>
2018-10-31 15:22:35 -0600

    re-add the password host route

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,7 +1,8 @@
 const config = {
   apiUrl: process.env.OC_API_URL,
+  backendHostUrl: process.env.OC_BACKEND_HOST,
   googleKey: process.env.OC_GOOGLE_KEY,
-  facebookKey: process.env.OC_FACEBOOK_KEY
+  facebookKey: process.env.OC_FACEBOOK_KEY,
 };
 
 export default config;
